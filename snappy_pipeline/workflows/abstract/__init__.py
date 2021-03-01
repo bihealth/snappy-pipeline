ffaa8393db5b0ea94de813d04b38fbf18190fc91
Eudes Barbosa <eudes.barbosa@bihealth.de>
2021-03-01 16:01:29 +0100

    Fixed bug in search.

--- snappy_pipeline/workflows/abstract/__init__.py
+++ snappy_pipeline/workflows/abstract/__init__.py
@@ -815,10 +815,10 @@ class LinkInPathGenerator:
         # Assumption: sheet file stored in the same level as config file,
         # i.e., one of the base paths.
         for base_p in base_paths:
-            dir_path = os.path.dirname(os.path.realpath(base_p))
+            dir_path = os.path.realpath(base_p)
             # Find all files
             for item in os.listdir(dir_path):
-                if os.path.isfile(os.path.join(dir_path, item)) and sheet_file_name == item:
+                if sheet_file_name == item:
                     return os.path.join(dir_path, sheet_file_name)
         # If not found: None
         return None
