af631bf61bf97ecc505bd163cf9c624434158277
Eudes Barbosa <eudes.barbosa@bihealth.de>
2021-04-07 13:34:05 +0200

    Solved plynt issue.

--- snappy_pipeline/workflows/somatic_wgs_cnv_calling/__init__.py
+++ snappy_pipeline/workflows/somatic_wgs_cnv_calling/__init__.py
@@ -216,7 +216,7 @@ class SomaticWgsCnvCallingStepPart(BaseStepPart):
         )
 
     @dictify
-    def _get_log_file(self, action):
+    def _get_log_file(self, _action):
         """Return path to log file"""
         token = "{{mapper}}.{var_caller}.{{cancer_library}}".format(var_caller=self.__class__.name)
         prefix = "work/{token}/log/{token}".format(token=token)
