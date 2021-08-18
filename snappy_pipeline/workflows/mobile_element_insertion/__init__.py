899fbdab3bf44d81ea06d307ddcc53c142f4a5c0
Eudes Barbosa <eudes.barbosa@bihealth.de>
2021-08-18 15:18:40 +0200

    MEI detection workflow scaffold.

---
+++ snappy_pipeline/workflows/mobile_element_insertion/__init__.py
@@ -0,0 +1,263 @@
+# -*- coding: utf-8 -*
+import os
+
+from biomedsheets.shortcuts import GermlineCaseSheet, is_not_background
+from snakemake.io import expand
+
+from snappy_pipeline.base import UnsupportedActionException
+from snappy_pipeline.utils import dictify, listify
+from snappy_pipeline.workflows.abstract import BaseStep, BaseStepPart, LinkOutStepPart
+from snappy_pipeline.workflows.ngs_mapping import NgsMappingWorkflow
+
+#: Extensions of files to create as main payload.
+EXT = (".json", ".json.md5")
+
+
+#: Default configuration for the mobile_element_insertion step.
+DEFAULT_CONFIG = r"""
+# Default configuration mobile_element_insertion
+step_config:
+  mobile_element_insertion:
+    # Path to Scramble installation directory
+    scramble_install_dir: REQUIRED
+    # Path to the ngs_mapping step
+    path_ngs_mapping: ../ngs_mapping
+"""
+
+
+class ScrambleStepPart(BaseStepPart):
+    """Mobile element insertion detection using GeneDx::scramble"""
+
+    #: Step name.
+    name = "scramble"
+
+    #: Valid actions.
+    actions = ("cluster", "analysis", "annotate")
+
+    def get_input_files(self, action):
+        """Return input function for scramble rules.
+
+        :param action: Action (i.e., step) in the workflow.
+        :type action: str
+
+        :return: Returns input function for scramble rule based on inputted action.
+
+        :raises UnsupportedActionException: if action not in class defined list of valid actions.
+        """
+        # Validate inputted action
+        if action not in self.actions:
+            valid_actions_str = ", ".join(self.actions)
+            error_message = "Action '{action}' is not supported. Valid options: {options}".format(
+                action=action, options=valid_actions_str
+            )
+            raise UnsupportedActionException(error_message)
+        # Return requested function
+        return getattr(self, "_get_input_files_{}".format(action))
+
+    def get_output_files(self, action):
+        """Return output function for scramble rules.
+
+        :param action: Action (i.e., step) in the workflow.
+        :type action: str
+
+        :return: Returns output function for scramble rule based on inputted action.
+
+        :raises UnsupportedActionException: if action not in class defined list of valid actions.
+        """
+        # Validate inputted action
+        if action not in self.actions:
+            valid_actions_str = ", ".join(self.actions)
+            error_message = "Action '{action}' is not supported. Valid options: {options}".format(
+                action=action, options=valid_actions_str
+            )
+            raise UnsupportedActionException(error_message)
+        return getattr(self, "_get_output_files_{}".format(action))()
+
+    @listify
+    def _get_input_files_cluster(self, wildcards):
+        """Yield BAM files based on subworkflow `ngs_mapping` results.
+
+        :param wildcards: Snakemake rule wildcards.
+        :type wildcards: snakemake.io.Wildcards
+        """
+        ngs_mapping = self.parent.sub_workflows["ngs_mapping"]
+        bam_tpl = "output/{mapper}.{library_name}/out/{mapper}.{library_name}.bam"
+        yield ngs_mapping(bam_tpl.format(**wildcards))
+
+    @staticmethod
+    @listify
+    def _get_input_files_analysis(wildcards):
+        """Yield input files' pattern for rule `annotate` - based on scramble call results.
+
+        :param wildcards: Snakemake rule wildcards.
+        :type wildcards: snakemake.io.Wildcards
+        """
+        name_pattern = "{mapper}.scramble.{library_name}"
+        base_name_out = "work/{name_pattern}/out/{name_pattern}.{ext}".format(
+            name_pattern=name_pattern, ext="txt"
+        )
+        yield base_name_out.format(**wildcards)
+
+    @staticmethod
+    @listify
+    def _get_input_files_annotate(wildcards):
+        """Yield input files' pattern for rule `annotate` - based on scramble results.
+
+        :param wildcards: Snakemake rule wildcards.
+        :type wildcards: snakemake.io.Wildcards
+        """
+        name_pattern = "{mapper}.scramble.{library_name}"
+        base_name_out = "work/{name_pattern}/out/{name_pattern}{ext}".format(
+            name_pattern=name_pattern, ext="_MEIs.txt"
+        )
+        yield base_name_out.format(**wildcards)
+
+    @staticmethod
+    @dictify
+    def _get_output_files_cluster():
+        """Yield output files' patterns for scramble cluster call."""
+        # Initialise variables
+        name_pattern = "{mapper}.scramble.{library_name}"
+        ext = "txt"
+        # Yield
+        yield ext, "work/{name_pattern}/out/{name_pattern}.{ext}".format(
+            name_pattern=name_pattern, ext=ext
+        )
+
+    @staticmethod
+    @dictify
+    def _get_output_files_analysis():
+        """Yield output files' patterns for scramble call."""
+        # Initialise variables
+        name_pattern = "{mapper}.scramble.{library_name}"
+        ext_dict = {"txt": "_MEIs.txt"}
+        # Yield
+        for key, ext in ext_dict.items():
+            yield key, "work/{name_pattern}/out/{name_pattern}{ext}".format(
+                name_pattern=name_pattern, ext=ext
+            )
+
+    @staticmethod
+    @dictify
+    def _get_output_files_annotate():
+        """Yield output files' patterns for rule `annotate`."""
+        # Initialise variables
+        name_pattern = "{mapper}.scramble_annotated.{library_name}"
+        ext_dict = {"json": "json", "json_md5": "json.md5"}
+        # Yield
+        for key, ext in ext_dict.items():
+            yield key, "work/{name_pattern}/out/{name_pattern}.{ext}".format(
+                name_pattern=name_pattern, ext=ext
+            )
+
+    def get_log_file(self, action):
+        """Return log function for scramble rules.
+
+        :param action: Action (i.e., step) in the workflow.
+        :type action: str
+
+        :return: Returns log function for scramble rule based on inputted action.
+
+        :raises UnsupportedActionException: if action not in class defined list of valid actions.
+        """
+        # Initialise variable
+        name_pattern = "{mapper}.scramble.{library_name}"
+
+        # Validate inputted action
+        if action not in self.actions:
+            valid_actions_str = ", ".join(self.actions)
+            error_message = "Action '{action}' is not supported. Valid options: {options}".format(
+                action=action, options=valid_actions_str
+            )
+            raise UnsupportedActionException(error_message)
+        if action == "annotate":
+            name_pattern_annotated = "{mapper}.scramble_annotated.{library_name}"
+            return "work/{name_pattern}/log/{name_pattern}.log".format(
+                name_pattern=name_pattern_annotated
+            )
+        return "work/{name_pattern}/log/{name_pattern}_{action}.log".format(
+            name_pattern=name_pattern, action=action
+        )
+
+
+class MEIWorkflow(BaseStep):
+    """Perform germline mobile element insertion detection."""
+
+    #: Workflow name
+    name = "mobile_element_insertion"
+
+    #: Sample sheet shortcut class
+    sheet_shortcut_class = GermlineCaseSheet
+
+    def __init__(
+        self, workflow, config, cluster_config, config_lookup_paths, config_paths, workdir
+    ):
+        super().__init__(
+            workflow,
+            config,
+            cluster_config,
+            config_lookup_paths,
+            config_paths,
+            workdir,
+            (NgsMappingWorkflow,),
+        )
+        # Register sub step classes so the sub steps are available
+        self.register_sub_step_classes((LinkOutStepPart, ScrambleStepPart))
+        # Register sub workflows
+        self.register_sub_workflow("ngs_mapping", self.config["path_ngs_mapping"])
+
+    @classmethod
+    def default_config_yaml(cls):
+        """Return default config YAML, to be overwritten by project-specific one"""
+        return DEFAULT_CONFIG
+
+    @listify
+    def _all_donors(self, include_background=True):
+        """Return list of all donors in sample sheet."""
+        sheets = self.shortcut_sheets
+        if not include_background:
+            sheets = list(filter(is_not_background, sheets))
+        for sheet in sheets:
+            for pedigree in sheet.cohort.pedigrees:
+                yield from pedigree.donors
+
+    @listify
+    def get_result_files(self):
+        """Get list of result files.
+
+        :return: Return list of result files for the germline mobile element insertion
+        detection workflow.
+        """
+        # Initialise variable
+        tools = ("scramble",)
+        name_pattern = "{mapper}.{tool}_annotated.{donor.dna_ngs_library.name}"
+        yield from self._yield_result_files(
+            os.path.join("output", name_pattern, "out", name_pattern + "{ext}"),
+            mapper=self.w_config["step_config"]["ngs_mapping"]["tools"]["dna"],
+            tool=tools,
+            ext=EXT,
+        )
+
+    def _yield_result_files(self, tpl, **kwargs):
+        """Build output paths from path template and extension list."""
+        for donor in self._all_donors(include_background=False):
+            if donor.dna_ngs_library:  # ignores samples without DNA library
+                yield from expand(tpl, donor=[donor], **kwargs)
+
+    def check_config(self):
+        """Check that the necessary configuration is available for the step"""
+        # Requires path to ngs_mapping output, i.e., the BAM files
+        self.ensure_w_config(
+            config_keys=("step_config", "mobile_element_insertion", "path_ngs_mapping"),
+            msg=(
+                "Path to NGS mapping not configured but required for mobile "
+                "element insertion detection."
+            ),
+        )
+        # Requires path to ngs_mapping output, i.e., the BAM files
+        self.ensure_w_config(
+            config_keys=("step_config", "mobile_element_insertion", "scramble_install_dir"),
+            msg=(
+                "Path to scramble installation directory not configured but required for detection."
+            ),
+        )
---
+++ tests/snappy_pipeline/workflows/test_workflows_mobile_element_insertion.py
@@ -0,0 +1,197 @@
+# -*- coding: utf-8 -*-
+"""Tests for the mobile_element_insertion workflow module code"""
+
+import textwrap
+
+import pytest
+import ruamel.yaml as yaml
+from snakemake.io import Wildcards
+
+from snappy_pipeline.workflows.mobile_element_insertion import MEIWorkflow
+
+from .conftest import patch_module_fs
+
+
+@pytest.fixture(scope="module")  # otherwise: performance issues
+def minimal_config():
+    """Return YAML parsing result for (germline) configuration"""
+    return yaml.round_trip_load(
+        textwrap.dedent(
+            r"""
+        static_data_config:
+          reference:
+            path: /path/to/ref.fa
+        step_config:
+          ngs_mapping:
+            tools:
+              dna: ['bwa']
+            bwa:
+              path_index: /path/to/bwa/index.fasta
+        data_sets:
+          first_batch:
+            file: sheet.tsv
+            search_patterns:
+            - {'left': '*/*/*_R1.fastq.gz', 'right': '*/*/*_R2.fastq.gz'}
+            search_paths: ['/path']
+            type: germline_variants
+            naming_scheme: only_secondary_id
+        """
+        ).lstrip()
+    )
+
+
+@pytest.fixture
+def mei_workflow(
+    dummy_workflow,
+    minimal_config,
+    dummy_cluster_config,
+    config_lookup_paths,
+    work_dir,
+    config_paths,
+    germline_sheet_fake_fs,
+    mocker,
+):
+    """Return MEIWorkflow object pre-configured with germline sheet"""
+    # Patch out file-system related things in abstract (the crawling link in step is defined there)
+    patch_module_fs("snappy_pipeline.workflows.abstract", germline_sheet_fake_fs, mocker)
+    # Update the "globals" attribute of the mock workflow (snakemake.workflow.Workflow) so we
+    # can obtain paths from the function as if we really had a NGSMappingPipelineStep here
+    dummy_workflow.globals = {"ngs_mapping": lambda x: "NGS_MAPPING/" + x}
+    # Construct the workflow object
+    return MEIWorkflow(
+        dummy_workflow,
+        minimal_config,
+        dummy_cluster_config,
+        config_lookup_paths,
+        config_paths,
+        work_dir,
+    )
+
+
+def test_mei_workflow_files(mei_workflow):
+    """Tests MEIWorkflow::get_result_files()
+    Tests simple functionality of the workflow: checks if file structure is created according
+    to the expected results for scramble.
+    """
+    # Define expected
+    pattern_out = (
+        "output/bwa.scramble_annotated.P00{i}-N1-DNA1-WGS1/out/"
+        "bwa.scramble_annotated.P00{i}-N1-DNA1-WGS1.{ext}"
+    )
+    expected = [
+        pattern_out.format(i=i, ext=ext)
+        for i in range(1, 7)  # all donors: P001 - P006
+        for ext in (
+            "json",
+            "json.md5",
+        )
+    ]
+    # Get actual
+    actual = mei_workflow.get_result_files()
+    assert sorted(actual) == sorted(expected)
+
+
+# Tests for ScrambleStepPart (cluster) -------------------------------------------------------------
+
+
+def test_scramble_cluster_step_part_get_input_files(mei_workflow):
+    """Tests ScrambleStepPart::_get_input_files_cluster()"""
+    # Define expected
+    expected = ["NGS_MAPPING/output/bwa.P001-N1-DNA1-WGS1/out/bwa.P001-N1-DNA1-WGS1.bam"]
+    # Get actual
+    wildcards = Wildcards(fromdict={"mapper": "bwa", "library_name": "P001-N1-DNA1-WGS1"})
+    actual = mei_workflow.get_input_files("scramble", "cluster")(wildcards)
+    assert actual == expected
+
+
+def test_scramble_cluster_step_part_get_output_files(mei_workflow):
+    """Tests ScrambleStepPart::_get_output_files_cluster()"""
+    # Define expected
+    pattern_out = "work/{mapper}.scramble.{library_name}/out/{mapper}.scramble.{library_name}"
+    expected = {"txt": pattern_out + ".txt"}
+    # Get actual
+    actual = mei_workflow.get_output_files("scramble", "cluster")
+    assert actual == expected
+
+
+def test_scramble_cluster_step_part_get_log_file(mei_workflow):
+    """Tests ScrambleStepPart::_get_log_files_cluster()"""
+    # Define expected
+    expected = (
+        "work/{mapper}.scramble.{library_name}/log/{mapper}.scramble.{library_name}_cluster.log"
+    )
+    # Get actual
+    actual = mei_workflow.get_log_file("scramble", "cluster")
+    assert actual == expected
+
+
+# Tests for ScrambleStepPart (analysis) ------------------------------------------------------------
+
+
+def test_scramble_analysis_step_part_get_input_files(mei_workflow):
+    """Tests ScrambleStepPart::_get_input_files_analysis()"""
+    # Define expected
+    expected = ["work/bwa.scramble.P001-N1-DNA1-WGS1/out/bwa.scramble.P001-N1-DNA1-WGS1.txt"]
+    # Get actual
+    wildcards = Wildcards(fromdict={"mapper": "bwa", "library_name": "P001-N1-DNA1-WGS1"})
+    actual = mei_workflow.get_input_files("scramble", "analysis")(wildcards)
+    assert actual == expected
+
+
+def test_scramble_analysis_step_part_get_output_files(mei_workflow):
+    """Tests ScrambleStepPart::_get_output_files_analysis()"""
+    # Define expected
+    pattern_out = "work/{mapper}.scramble.{library_name}/out/{mapper}.scramble.{library_name}"
+    expected = {"txt": pattern_out + "_MEIs.txt"}
+    # Get actual
+    actual = mei_workflow.get_output_files("scramble", "analysis")
+    assert actual == expected
+
+
+def test_scramble_analysis_step_part_get_log_file(mei_workflow):
+    """Tests ScrambleStepPart::_get_log_files_analysis()"""
+    # Define expected
+    expected = (
+        "work/{mapper}.scramble.{library_name}/log/{mapper}.scramble.{library_name}_analysis.log"
+    )
+    # Get actual
+    actual = mei_workflow.get_log_file("scramble", "analysis")
+    assert actual == expected
+
+
+# Tests for ScrambleStepPart (annotate) ------------------------------------------------------------
+
+
+def test_scramble_annotate_step_part_get_input_files(mei_workflow):
+    """Tests ScrambleStepPart::_get_input_files_annotate()"""
+    # Define expected
+    expected = ["work/bwa.scramble.P001-N1-DNA1-WGS1/out/bwa.scramble.P001-N1-DNA1-WGS1_MEIs.txt"]
+    # Get actual
+    wildcards = Wildcards(fromdict={"mapper": "bwa", "library_name": "P001-N1-DNA1-WGS1"})
+    actual = mei_workflow.get_input_files("scramble", "annotate")(wildcards)
+    assert actual == expected
+
+
+def test_scramble_annotate_step_part_get_output_files(mei_workflow):
+    """Tests ScrambleStepPart::_get_output_files_annotate()"""
+    # Define expected
+    pattern_out = (
+        "work/{mapper}.scramble_annotated.{library_name}/out/"
+        "{mapper}.scramble_annotated.{library_name}"
+    )
+    expected = {"json": pattern_out + ".json", "json_md5": pattern_out + ".json.md5"}
+    # Get actual
+    actual = mei_workflow.get_output_files("scramble", "annotate")
+    assert actual == expected
+
+
+def test_scramble_annotate_step_part_get_log_file(mei_workflow):
+    """Tests ScrambleStepPart::_get_log_files_annotate()"""
+    # Define expected
+    expected = (
+        "work/{mapper}.scramble_annotated.{library_name}/log/"
+        "{mapper}.scramble_annotated.{library_name}.log"
+    )
+    # Get actual
+    actual = mei_workflow.get_log_file("scramble", "annotate")
+    assert actual == expected
