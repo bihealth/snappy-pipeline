70d74c87caceb9ea554ff893cde81577df0aa3ca
Eudes Barbosa <eudes.barbosa@charite.de>
2021-10-28 13:58:01 +0200

    Add repeat expansion analysis (#76)
    * New workflow `repeat_analysis` to investigate repeat expansions. Currently only using Illumina::ExpansionHunter

---
+++ tests/snappy_pipeline/workflows/test_workflows_repeat_expansion.py
@@ -0,0 +1,193 @@
+# -*- coding: utf-8 -*-
+"""Tests for the repeat_expansion workflow module code"""
+
+import textwrap
+
+import pytest
+import ruamel.yaml as yaml
+from snakemake.io import Wildcards
+
+from snappy_pipeline.workflows.repeat_expansion import RepeatExpansionWorkflow
+
+from .conftest import patch_module_fs
+
+
+@pytest.fixture(scope="module")  # otherwise: performance issues
+def minimal_config():
+    """Return YAML parsing result for (germline) configuration"""
+    return yaml.round_trip_load(
+        textwrap.dedent(
+            r"""
+        static_data_config:
+          reference:
+            path: /path/to/ref.fa
+
+        step_config:
+          ngs_mapping:
+            tools:
+              dna: ['bwa']
+            bwa:
+              path_index: /path/to/bwa/index.fasta
+
+        data_sets:
+          first_batch:
+            file: sheet.tsv
+            search_patterns:
+            - {'left': '*/*/*_R1.fastq.gz', 'right': '*/*/*_R2.fastq.gz'}
+            search_paths: ['/path']
+            type: germline_variants
+            naming_scheme: only_secondary_id
+        """
+        ).lstrip()
+    )
+
+
+@pytest.fixture
+def repeat_expansion_workflow(
+    dummy_workflow,
+    minimal_config,
+    dummy_cluster_config,
+    config_lookup_paths,
+    work_dir,
+    config_paths,
+    germline_sheet_fake_fs,
+    mocker,
+):
+    """Return RepeatExpansionWorkflow object pre-configured with germline sheet"""
+    # Patch out file-system related things in abstract (the crawling link in step is defined there)
+    patch_module_fs("snappy_pipeline.workflows.abstract", germline_sheet_fake_fs, mocker)
+    # Update the "globals" attribute of the mock workflow (snakemake.workflow.Workflow) so we
+    # can obtain paths from the function as if we really had a NGSMappingPipelineStep here
+    dummy_workflow.globals = {"ngs_mapping": lambda x: "NGS_MAPPING/" + x}
+    # Construct the workflow object
+    return RepeatExpansionWorkflow(
+        dummy_workflow,
+        minimal_config,
+        dummy_cluster_config,
+        config_lookup_paths,
+        config_paths,
+        work_dir,
+    )
+
+
+def test_repeat_expansion_workflow_files(repeat_expansion_workflow):
+    """Tests RepeatExpansionWorkflow.get_result_files()
+
+    Tests simple functionality of the workflow: checks if file structure is created according
+    to the expected results for ExpansionHunter.
+    """
+    # Define expected
+    pattern_json_out = (
+        "output/bwa.expansionhunter_annotated.P00{i}-N1-DNA1-WGS1/out/"
+        "bwa.expansionhunter_annotated.P00{i}-N1-DNA1-WGS1.{ext}"
+    )
+    pattern_vcf_out = (
+        "output/bwa.expansionhunter.P00{i}-N1-DNA1-WGS1/out/"
+        "bwa.expansionhunter.P00{i}-N1-DNA1-WGS1.{ext}"
+    )
+    expected = [
+        pattern_json_out.format(i=i, ext=ext)
+        for i in range(1, 7)  # all donors: P001 - P006
+        for ext in (
+            "json",
+            "json.md5",
+        )
+    ]
+    expected += [
+        pattern_vcf_out.format(i=i, ext=ext)
+        for i in range(1, 7)  # all donors: P001 - P006
+        for ext in (
+            "vcf",
+            "vcf.md5",
+        )
+    ]
+    # Get actual
+    actual = repeat_expansion_workflow.get_result_files()
+    assert sorted(actual) == sorted(expected)
+
+
+# Tests for ExpansionHunterStepPart (run) ----------------------------------------------------------
+
+
+def test_expansionhunter_run_step_part_get_input_files(repeat_expansion_workflow):
+    """Tests ExpansionHunterStepPart._get_input_files_run()"""
+    # Define expected
+    expected = ["NGS_MAPPING/output/bwa.P001-N1-DNA1-WGS1/out/bwa.P001-N1-DNA1-WGS1.bam"]
+    # Get actual
+    wildcards = Wildcards(fromdict={"mapper": "bwa", "library_name": "P001-N1-DNA1-WGS1"})
+    actual = repeat_expansion_workflow.get_input_files("expansionhunter", "run")(wildcards)
+    assert actual == expected
+
+
+def test_expansionhunter_run_step_part_get_output_files(repeat_expansion_workflow):
+    """Tests ExpansionHunterStepPart._get_output_files_run()"""
+    # Define expected
+    pattern_out = (
+        "work/{mapper}.expansionhunter.{library_name}/out/"
+        "{mapper}.expansionhunter.{library_name}"
+    )
+    expected = {
+        "json": pattern_out + ".json",
+        "vcf": pattern_out + ".vcf",
+        "vcf_md5": pattern_out + ".vcf.md5",
+    }
+    # Get actual
+    actual = repeat_expansion_workflow.get_output_files("expansionhunter", "run")
+    assert actual == expected
+
+
+def test_expansionhunter_run_step_part_get_log_file(repeat_expansion_workflow):
+    """Tests RepeatExpansionWorkflow._get_log_files_run()"""
+    # Define expected
+    expected = (
+        "work/{mapper}.expansionhunter.{library_name}/log/"
+        "{mapper}.expansionhunter.{library_name}.log"
+    )
+    # Get actual
+    actual = repeat_expansion_workflow.get_log_file("expansionhunter", "run")
+    assert actual == expected
+
+
+def test_expansionhunter_run_step_part_get_params(repeat_expansion_workflow):
+    """Tests RepeatExpansionWorkflow.get_params()"""
+    # P001
+    expected = "female"
+    wildcards = Wildcards(fromdict={"library_name": "P001-N1-DNA1-WGS1"})
+    actual = repeat_expansion_workflow.get_params("expansionhunter", "run")(wildcards)
+    assert actual.get("sex") == expected, "P001-N1-DNA1-WGS1 associated with a female patient."
+    # P002
+    expected = "male"
+    wildcards = Wildcards(fromdict={"library_name": "P002-N1-DNA1-WGS1"})
+    actual = repeat_expansion_workflow.get_params("expansionhunter", "run")(wildcards)
+    assert actual.get("sex") == expected, "P001-N1-DNA1-WGS1 associated with a male patient."
+
+
+# Tests for ExpansionHunterStepPart (annotate) -----------------------------------------------------
+
+
+def test_expansionhunter_annotate_step_part_get_input_files(repeat_expansion_workflow):
+    """Tests ExpansionHunterStepPart._get_input_files_annotate()"""
+    # Define expected
+    json_out = (
+        "work/{mapper}.expansionhunter.{library_name}/out/"
+        "{mapper}.expansionhunter.{library_name}.json"
+    )
+    expected = [json_out]
+
+    # Get actual
+    wildcards = Wildcards(fromdict={"mapper": "bwa", "library_name": "P001-N1-DNA1-WGS1"})
+    actual = repeat_expansion_workflow.get_input_files("expansionhunter", "annotate")(wildcards)
+    assert actual == expected
+
+
+def test_expansionhunter_annotate_step_part_get_output_files(repeat_expansion_workflow):
+    """Tests ExpansionHunterStepPart._get_output_files_annotate()"""
+    # Define expected
+    json_out = (
+        "work/{mapper}.expansionhunter_annotated.{library_name}/out/"
+        "{mapper}.expansionhunter_annotated.{library_name}"
+    )
+    expected = {"json": json_out + ".json", "json_md5": json_out + ".json.md5"}
+    # Get actual
+    actual = repeat_expansion_workflow.get_output_files("expansionhunter", "annotate")
+    assert actual == expected
