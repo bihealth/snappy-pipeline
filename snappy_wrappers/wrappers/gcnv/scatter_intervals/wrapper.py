819f379661dc6f2c0396d95d9fe4ba214abd8d57
Eudes Barbosa <eudes.barbosa@charite.de>
2022-04-13 13:40:45 +0200

    Removed gCNV model subworkflow for simplification - incorporated into gcnv_cohort_mode.rules

--- snappy_wrappers/wrappers/gcnv/scatter_intervals/wrapper.py
+++ snappy_wrappers/wrappers/gcnv/scatter_intervals/wrapper.py
@@ -2,14 +2,6 @@
 
 from snakemake.shell import shell
 
-# Filter interval list file from gCNV model input
-interval_list = [
-    path
-    for path in snakemake.input
-    if str(path).endswith(".interval_list")
-    and snakemake.wildcards.mapper in str(path)  # Check for correct mapper
-    and snakemake.wildcards.library_kit in str(path)  # Check for correct library kit
-]
 
 shell(
     r"""
@@ -21,7 +13,7 @@ rm -rf "{snakemake.output}" && mkdir -p "{snakemake.output}"
 trap "rm -rf {snakemake.output}" ERR
 
 gatk IntervalListTools \
-    --INPUT {interval_list} \
+    --INPUT {snakemake.input.interval_list} \
     --SUBDIVISION_MODE INTERVAL_COUNT \
     --SCATTER_CONTENT 5000 \
     --OUTPUT {snakemake.output}
