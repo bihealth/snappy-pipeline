562d5d805d45046f766235cbded6af711440ed79
Eudes Barbosa <eudes.barbosa@charite.de>
2022-08-31 10:26:47 +0200

    Removed unused variables from wrapper gcnv/filter_intervals

--- snappy_wrappers/wrappers/gcnv/filter_intervals/wrapper.py
+++ snappy_wrappers/wrappers/gcnv/filter_intervals/wrapper.py
@@ -1,28 +1,6 @@
 # -*- coding: utf-8 -*-
-# isort:skip_file
-from snappy_pipeline.utils import DictQuery
-import os
 
 from snakemake.shell import shell
-
-
-# Pick the target BED file to use.  If it goes by the name "default" then we
-# simply take the one at xhmm/path_target_interval_list.  Otherwise, we
-# have to go through the list in xhmm/path_target_interval_list_mapping.
-gcnv_config = DictQuery(snakemake.config).get("step_config/targeted_seq_cnv_calling/gcnv")
-if snakemake.wildcards.library_kit == "default":
-    target_interval_bed = gcnv_config["path_target_interval_list"]
-else:
-    for item in gcnv_config["path_target_interval_list_mapping"]:
-        if item["name"] == snakemake.wildcards.library_kit:
-            target_interval_bed = item["path"]
-            break
-    else:  # of for, did not break out
-        raise Exception("Found no target intervals for %s" % item["name"])
-
-map_bed = DictQuery(snakemake.config).get(
-    "step_config/targeted_seq_cnv_calling/gcnv/path_uniquely_mapable_bed"
-)
 
 paths_cov = " ".join(snakemake.input.covs)
 
@@ -31,7 +9,7 @@ shell(
 set -x
 
 gatk FilterIntervals \
-    -L {snakemake.input.interval_list} \
+    --intervals {snakemake.input.interval_list} \
     --annotated-intervals {snakemake.input.tsv} \
     --interval-merging-rule OVERLAPPING_ONLY \
     $(for tsv in {paths_cov}; do echo -I $tsv; done) \
@@ -39,6 +17,6 @@ gatk FilterIntervals \
     --maximum-gc-content 0.9 \
     --minimum-mappability 0.9 \
     --maximum-mappability 1.0 \
-    -O {snakemake.output.interval_list}
+    --output {snakemake.output.interval_list}
 """
 )
