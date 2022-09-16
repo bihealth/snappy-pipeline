6a9300588ff897e068fc690cc79e048c527ce6fb
Eric Blanc <eric.blanc@bihealth.de>
2022-09-16 11:16:29 +0200

    Added arriba to somatic_gene_fusion_calling step

--- snappy_pipeline/workflows/somatic_gene_fusion_calling/__init__.py
+++ snappy_pipeline/workflows/somatic_gene_fusion_calling/__init__.py
@@ -41,6 +41,7 @@ import os
 from biomedsheets.shortcuts import CancerCaseSheet, CancerCaseSheetOptions
 from snakemake.io import touch
 
+from snappy_pipeline.base import InvalidConfiguration
 from snappy_pipeline.utils import dictify, listify
 from snappy_pipeline.workflows.abstract import (
     BaseStep,
@@ -78,6 +79,30 @@ step_config:
       path_ctat_resource_lib: REQUIRED
     defuse:
       path_dataset_directory: REQUIRED
+    arriba:
+      path_index: REQUIRED       # STAR path index (preferably 2.7.10 or later)
+      features: REQUIRED         # Gene features (for ex. ENCODE or ENSEMBL) in gtf format
+      blacklist: ""              # optional (provided in the arriba distribution, see /fast/work/groups/cubi/projects/biotools/static_data/app_support/arriba/v2.3.0)
+      known_fusions: ""          # optional
+      tags: ""                   # optional (can be set to the same path as known_fusions)
+      structural_variants: ""    # optional
+      protein_domains: ""        # optional
+      num_threads: 8
+      trim_adapters: false
+      num_threads_trimming: 4
+      star_parameters:
+      - " --outFilterMultimapNmax 50"
+      - " --peOverlapNbasesMin 10"
+      - " --alignSplicedMateMapLminOverLmate 0.5"
+      - " --alignSJstitchMismatchNmax 5 -1 5 5"
+      - " --chimSegmentMin 10"
+      - " --chimOutType WithinBAM HardClip"
+      - " --chimJunctionOverhangMin 10"
+      - " --chimScoreDropMax 30"
+      - " --chimScoreJunctionNonGTAG 0"
+      - " --chimScoreSeparation 1"
+      - " --chimSegmentReadGapMax 3"
+      - " --chimMultimapNmax 50"
 """.lstrip()
 
 
@@ -116,6 +141,15 @@ class SomaticGeneFusionCallingStepPart(BaseStepPart):
         self._validate_action(action)
         return "work/{name}.{{library_name}}/log/snakemake.gene_fusion_calling.log".format(
             name=self.name
+        )
+
+    @listify
+    def get_result_files(self):
+        return (
+            "output/{name}.{{library_name}}/out/.done".format(name=self.name),
+            "output/{name}.{{library_name}}/log/snakemake.gene_fusion_calling.log".format(
+                name=self.name
+            ),
         )
 
     def _collect_reads(self, wildcards, library_name, prefix):
@@ -363,6 +397,104 @@ class HeraStepPart(SomaticGeneFusionCallingStepPart):
         )
 
 
+class ArribaStepPart(SomaticGeneFusionCallingStepPart):
+    """Somatic gene fusion calling from RNA-seq reads using arriba"""
+
+    #: Step name
+    name = "arriba"
+
+    def check_config(self):
+        """Check parameters in configuration.
+
+        Method checks that all parameters required to execute STAR & arriba are present in the
+        configuration. It further checks that the provided index has all the expected file
+        extensions. If invalid configuration, it raises InvalidConfiguration exception.
+        """
+        # Check if tool is at all included in workflow
+        if self.__class__.name not in self.config["tools"]:
+            return  # arriba not run, don't check configuration  # pragma: no cover
+
+        # Check required configuration settings present
+        self.parent.ensure_w_config(
+            config_keys=("step_config", "somatic_gene_fusion_calling", "arriba", "path_index"),
+            msg="Path to STAR indices is required",
+        )
+        self.parent.ensure_w_config(
+            config_keys=("step_config", "somatic_gene_fusion_calling", "arriba", "features"),
+            msg="Path to genomic features gtf file is required",
+        )
+
+        # Check that the path to the STAR index is valid.
+        for fn in ("Genome", "SA", "SAindex"):
+            expected_path = self.config["arriba"]["path_index"] + "/" + fn
+            if not os.path.exists(expected_path):  # pragma: no cover
+                tpl = "Expected STAR indices input path {expected_path} does not exist!".format(
+                    expected_path=expected_path
+                )
+                raise InvalidConfiguration(tpl)
+
+    def get_args(self, action):
+        """Return function that maps wildcards to dict for input files"""
+
+        def flatten(lst):
+            return [x for pair in lst for x in pair]
+
+        def args_function(wildcards):
+            # TODO: wildcards.library_name is tumor_library_name
+            left = list(sorted(self._collect_reads(wildcards, wildcards.library_name, "")))
+            right = list(sorted(self._collect_reads(wildcards, wildcards.library_name, "right-")))
+            return {"left": left, "right": right}
+
+        assert action == "run", "Unsupported actions"
+        return args_function
+
+    @dictify
+    def get_output_files(self, action):
+        self._validate_action(action)
+        base_path_out = "work/{name}.{{library_name}}/out/{name}.{{library_name}}.{ext}"
+        key_ext = (
+            ("fusions", "fusions.tsv"),
+            ("discarded", "discarded_fusions.tsv.gz"),
+        )
+        for key, ext in key_ext:
+            yield key, base_path_out.format(name=self.name, ext=ext)
+            yield key + "_md5", base_path_out.format(name=self.name, ext=ext) + ".md5"
+        yield "done", "work/arriba.{library_name}/out/.done"
+
+    @dictify
+    def get_log_file(self, action):
+        """Return dict of log files."""
+        _ = action
+        prefix = "work/{name}.{{library_name}}/log/{name}.{{library_name}}".format(name=self.name)
+        key_ext = (
+            ("log", ".log"),
+            ("conda_info", ".conda_info.txt"),
+            ("conda_list", ".conda_list.txt"),
+        )
+        for key, ext in key_ext:
+            yield key, prefix + ext
+            yield key + "_md5", prefix + ext + ".md5"
+
+    @listify
+    def get_result_files(self):
+        for path in self.get_output_files("run").values():
+            yield path.replace("work", "output")
+        for path in self.get_log_file("run").values():
+            yield path.replace("work", "output")
+
+    def get_resource_usage(self, action):
+        """Get resource Usage
+
+        :param action: Action (i.e., step) in the workflow, example: 'run'.
+        :type action: str
+
+        :return: Returns ResourceUsage for step.
+        """
+        # Validate action
+        self._validate_action(action)
+        return ResourceUsage(threads=8, time="24:00:00", memory=f"{64 * 1024}M")  # 1 day
+
+
 class SomaticGeneFusionCallingWorkflow(BaseStep):
     """Perform somatic gene fusion calling"""
 
@@ -393,6 +525,7 @@ class SomaticGeneFusionCallingWorkflow(BaseStep):
                 HeraStepPart,
                 StarFusionStepPart,
                 DefuseStepPart,
+                ArribaStepPart,
                 LinkInStep,
                 LinkOutStepPart,
             )
@@ -405,8 +538,9 @@ class SomaticGeneFusionCallingWorkflow(BaseStep):
         We will process all NGS libraries of all test samples in all sample
         sheets.
         """
-        name_pattern = "{fusion_caller}.{ngs_library.name}"
         for fusion_caller in self.config["tools"]:
+            sub_step = self.sub_steps[fusion_caller]
+            name_patterns = sub_step.get_result_files()
             for sheet in self.shortcut_sheets:
                 for donor in sheet.donors:
                     for bio_sample in donor.bio_samples.values():
@@ -414,10 +548,8 @@ class SomaticGeneFusionCallingWorkflow(BaseStep):
                             ngs_library = bio_sample.rna_ngs_library
                             if ngs_library is None:
                                 break
-                            name_pattern_value = name_pattern.format(
-                                fusion_caller=fusion_caller, ngs_library=ngs_library
-                            )
-                            yield os.path.join("output", name_pattern_value, "out", ".done")
+                            for name_pattern in name_patterns:
+                                yield name_pattern.format(library_name=ngs_library.name)
 
     def check_config(self):
         """Check that the required configurations are present."""
