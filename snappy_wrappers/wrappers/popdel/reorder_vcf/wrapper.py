3353a5a9c2c4227fd7f7b3d3376151d5c103adc9
Manuel Holtgrewe <manuel.holtgrewe@bihealth.de>
2023-01-23 20:16:59 +0100

    fix: popdel wrappers

--- snappy_wrappers/wrappers/popdel/reorder_vcf/wrapper.py
+++ snappy_wrappers/wrappers/popdel/reorder_vcf/wrapper.py
@@ -1,21 +1,56 @@
-# -*- coding: utf-8 -*-
-"""Wrapper for pedigree extraction from popdel cohort-wide call step."""
-
 from snakemake.shell import shell
 
-__author__ = "Manuel Holtgrewe"
-__email__ = "manuel.holtgrewe@bih-charite.de"
+__author__ = "Manuel Holtgrewe <manuel.holtgrewe@bih-charite.de>"
+
+DEF_HELPER_FUNCS = r"""
+compute-md5()
+{
+    if [[ $# -ne 2 ]]; then
+        >&2 echo "Invalid number of arguments: $#"
+        exit 1
+    fi
+    md5sum $1 \
+    | awk '{ gsub(/.*\//, "", $2); print; }' \
+    > $2
+}
+"""
 
 shell(
     r"""
-# -----------------------------------------------------------------------------
-# Redirect stderr to log file by default and enable printing executed commands
-exec &> >(tee -a "{snakemake.log}")
 set -x
-# -----------------------------------------------------------------------------
 
+# Write files for reproducibility -----------------------------------------------------------------
+
+{DEF_HELPER_FUNCS}
+
+# Write out information about conda and save a copy of the wrapper with picked variables
+# as well as the environment.yaml file.
+conda list >{snakemake.log.conda_list}
+conda info >{snakemake.log.conda_info}
+compute-md5 {snakemake.log.conda_list} {snakemake.log.conda_list_md5}
+compute-md5 {snakemake.log.conda_info} {snakemake.log.conda_info_md5}
+cp {__real_file__} {snakemake.log.wrapper}
+compute-md5 {snakemake.log.wrapper} {snakemake.log.wrapper_md5}
+cp $(dirname {__file__})/environment.yaml {snakemake.log.env_yaml}
+compute-md5 {snakemake.log.env_yaml} {snakemake.log.env_yaml_md5}
+
+# Also pipe stderr to log file --------------------------------------------------------------------
+
+if [[ -n "{snakemake.log.log}" ]]; then
+    if [[ "$(set +e; tty; set -e)" != "" ]]; then
+        rm -f "{snakemake.log.log}" && mkdir -p $(dirname {snakemake.log.log})
+        exec 2> >(tee -a "{snakemake.log.log}" >&2)
+    else
+        rm -f "{snakemake.log.log}" && mkdir -p $(dirname {snakemake.log.log})
+        echo "No tty, logging disabled" >"{snakemake.log.log}"
+    fi
+fi
+
+# Create auto-cleaned temporary directory
 export TMPDIR=$(mktemp -d)
 trap "rm -rf $TMPDIR" EXIT
+
+# Run actual tools --------------------------------------------------------------------------------
 
 echo '{snakemake.params.ped_members}' \
 | tr ' ' '\n' \
@@ -32,8 +67,26 @@ bcftools view \
 
 tabix -f {snakemake.output.vcf}
 
-pushd $(dirname {snakemake.output.vcf})
-md5sum $(basename {snakemake.output.vcf}) >$(basename {snakemake.output.vcf}).md5
-md5sum $(basename {snakemake.output.vcf}).tbi >$(basename {snakemake.output.vcf}).tbi.md5
+# Compute MD5 sums on output files
+compute-md5 {snakemake.output.vcf} {snakemake.output.vcf_md5}
+compute-md5 {snakemake.output.vcf_tbi} {snakemake.output.vcf_tbi_md5}
+
+# Create output links -----------------------------------------------------------------------------
+
+for path in {snakemake.output.output_links}; do
+  dst=$path
+  src=work/${{dst#output/}}
+  ln -sr $src $dst
+done
+"""
+)
+
+# Compute MD5 sums of logs.
+shell(
+    r"""
+{DEF_HELPER_FUNCS}
+
+sleep 1s  # try to wait for log file flush
+compute-md5 {snakemake.log.log} {snakemake.log.log_md5}
 """
 )
