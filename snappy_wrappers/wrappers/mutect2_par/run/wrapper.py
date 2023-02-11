dc0c51ce9ff19c68fe1637d66dea7fdd8c803521
Eric Blanc <eric.blanc@bihealth.de>
2023-02-11 11:24:54 +0100

    fix: compute log file md5 checksum

--- snappy_wrappers/wrappers/mutect2_par/run/wrapper.py
+++ snappy_wrappers/wrappers/mutect2_par/run/wrapper.py
@@ -6,10 +6,3 @@ from snakemake import shell
 
 # Kick off execution using the wrapper class defined above.
 ParallelMutect2Wrapper(snakemake).run().shutdown_logging()
-
-# Compute MD5 sums of logs.
-shell(
-    r"""
-md5sum {snakemake.log.log} > {snakemake.log.log_md5}
-"""
-)
