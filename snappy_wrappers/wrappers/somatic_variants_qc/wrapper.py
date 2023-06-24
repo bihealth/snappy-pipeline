ad07ebfc98e6a024b61724fa35151024eed9f9b7
giacuong171 <giacuong171@gmail.com>
2023-06-24 21:19:16 +0200

    Adding wrapper for somatic variants QC

---
+++ snappy_wrappers/wrappers/somatic_variants_qc/wrapper.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""Wrapper for summarize information of vcf file after
+somatic variant calling step
+"""
+from snakemake.shell import shell
+
+__author__ = "Pham Gia Cuong"
+__email__ = "pham.gia-cuong@bih-charite.de"
+
+shell(
+    r"""
+# -----------------------------------------------------------------------------
+# Redirect stderr to log file by default and enable printing executed commands
+exec 2> >(tee -a "{snakemake.log.log}")
+set -x
+# -----------------------------------------------------------------------------
+
+# Write out information about conda installation
+conda list > {snakemake.log.conda_list}
+conda info > {snakemake.log.conda_info}
+
+python summarize-vcf.py --rawvcf {snakemake.input.raw_vcf} \
+    --filtered-vcf {snakemake.input.passed_vcf} \
+    --exom-bedfile {snakemake.config[step_config][somatic_variants_QC][target_regions]} \
+    --padding {snakemake.config[step_config][somatic_variants_QC][padding]} \
+    --output {snakemake.output.json}
+
+pushd $(dirname {snakemake.output.json})
+md5sum $(basename {snakemake.output.json}) > $(basename {snakemake.output.json_md5})
+    """
+)
+
+# Compute MD5 sums of logs
+shell(
+    r"""
+md5sum {snakemake.log.log} > {snakemake.log.log_md5}
+md5sum {snakemake.log.conda_list} > {snakemake.log.conda_list_md5}
+md5sum {snakemake.log.conda_info} > {snakemake.log.conda_info_md5}
+"""
+)
