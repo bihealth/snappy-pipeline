753039a18d03006fc238589ba26c202d6208f1de
Eric Blanc <eric.blanc@bihealth.de>
2023-06-26 16:16:44 +0200

    meta_study merged with other meta files

--- snappy_wrappers/wrappers/cbioportal/meta_files/wrapper.py
+++ snappy_wrappers/wrappers/cbioportal/meta_files/wrapper.py
@@ -3,15 +3,16 @@
 See https://github.com/cBioPortal/cbioportal/blob/master/docs/File-Formats.md#clinical-data
 """
 
-from glob import glob
 import os
 
-target_dir = "work/upload"
 source_dir = os.path.abspath(os.path.dirname(__file__))
-study_id = snakemake.config["step_config"]["cbioportal_export"]["cancer_study_id"]
 
 # For each template, replace study_id and write to output in work dir
-for tpl in glob(os.path.join(source_dir, "meta*.txt")):
-    with open(tpl) as infile, open(os.path.join(target_dir, os.path.basename(tpl)), "w") as outfile:
+for fn in snakemake.output:
+    gn = os.path.join(source_dir, os.path.basename(fn))
+    assert os.path.exists(gn)
+    with open(gn, "rt") as infile, open(fn, "wt") as outfile:
         for line in infile:
-            outfile.write(line.replace("__CANCER_STUDY_IDENTIFIER__", study_id))
+            outfile.write(
+                line.format(**snakemake.config["step_config"]["cbioportal_export"]["study"])
+            )
