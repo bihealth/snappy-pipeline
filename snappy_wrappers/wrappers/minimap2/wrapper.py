fcd20f7a3a4c6db9273d49b3319f7b39e0cf9db3
Manuel Holtgrewe <manuel.holtgrewe@bih-charite.de>
2023-07-18 09:34:04 +0200

    wip

--- snappy_wrappers/wrappers/minimap2/wrapper.py
+++ snappy_wrappers/wrappers/minimap2/wrapper.py
@@ -15,7 +15,7 @@ shell(
     r"""
 set -x
 
-# Write out information about conda and save a copy of the wrapper with picked variables
+# Write out information about conda and save a copy of the wrapper with picked variables ----------
 # as well as the environment.yaml file.
 conda list >{snakemake.log.conda_list}
 conda info >{snakemake.log.conda_info}
@@ -37,31 +37,46 @@ if [[ -n "{snakemake.log.log}" ]]; then
     fi
 fi
 
+# Create auto-cleaned temporary directory ---------------------------------------------------------
 export TMPDIR=$(mktemp -d)
 trap "rm -rf $TMPDIR" EXIT
-mkdir -p $TMPDIR/{{out,sorted,sort.tmp}}
+
+# Determine preset --------------------------------------------------------------------------------
 
 if [[ "{library_kit}" == "PacBio HiFi" ]]; then
+    platform=PACBIO
     preset=map-hifi
 elif [[ "{library_kit}" == "PacBio CLR" ]]; then
+    platform=PACBIO
     preset=map-pb
-elif [[ "{library_kit}" == ONT* ]]; then
+elif [[ "{library_kit}" == guppy/* ]] || [[ "{library_kit}" == dorado/* ]]; then
+    platform=ONT
     preset=map-ont
 else
     >&2 echo "Unknown library kit {library_kit}"
     exit 1
 fi
 
-i=1
-for fname in $(find $(dirname {snakemake.input}) -name '*.bam' -or -name '*.fast?.gz'); do
-    basename=$(basename $fname .bam)
+# Function Definitions ----------------------------------------------------------------------------
 
-    if [[ "$fname" == *.bam ]]; then \
-        samtools fastq -F 2048 $fname; \
-    else \
-        zcat $fname; \
-    fi \
-    | minimap2 \
+# Under the assumption that we have single-ended reads, write out lines as FASTQ.
+input_files()
+{{
+    for fname in $(find $(dirname {snakemake.input}) -name '*.bam' -or -name '*.fast?.gz'); do
+        basename=$(basename $fname .bam)
+
+        if [[ "$fname" == *.bam ]]; then
+            samtools fastq -F 2048 $fname;
+        else \
+            zcat $fname;
+        fi
+    done
+}}
+
+# Actually run Minimap2.
+run_minimap2()
+{{
+    minimap2 \
         -t {snakemake.config[step_config][ngs_mapping][minimap2][mapping_threads]} \
         -x $preset \
         -a {snakemake.config[step_config][ngs_mapping][minimap2][path_index]} \
@@ -69,48 +84,63 @@ for fname in $(find $(dirname {snakemake.input}) -name '*.bam' -or -name '*.fast
         --MD \
         /dev/stdin \
     | samtools addreplacerg \
-        -r "@RG\tID:{snakemake.wildcards.library_name}.$i\tSM:{snakemake.wildcards.library_name}\tPL:PACBIO" - \
-    >$TMPDIR/out/$i.bam
+        -r "@RG\tID:{snakemake.wildcards.library_name}.$i\tSM:{snakemake.wildcards.library_name}\tPL:$PLATFORM" - \
+}}
 
-    samtools sort -m 4G -@ 3 \
+# Alignment postprocessing
+#
+# This function makes extensive use of tee-piping to prevent the BAM to be read from the disk
+# again for statistics.
+postproc_bam()
+{{
+    set -x
+
+    samtools sort \
+        -T $TMPDIR/sort_bam \
+        -m {snakemake.config[step_config][ngs_mapping][bwa_mem2][memory_bam_sort]} \
+        -@ {snakemake.config[step_config][ngs_mapping][bwa_mem2][num_threads_bam_sort]} \
         -O BAM \
-        -o $TMPDIR/sorted/$i.bam \
-        -T $TMPDIR/sort.tmp/ \
-        $TMPDIR/out/$i.bam
+        -o /dev/stdout \
+        /dev/stdin \
+    | tee >(samtools stats    /dev/stdin >{snakemake.output.report_bamstats_txt}) \
+    | tee >(samtools flagstat /dev/stdin >{snakemake.output.report_flagstats_txt}) \
+    | tee >(stdbuf -o0 samtools index /dev/stdin -o /dev/stdout >{snakemake.output.bam_bai}) \
+    | tee >(compute-md5       /dev/stdin {snakemake.output.bam_md5}) \
+    > {snakemake.output.bam}
 
-    let "i=$i+1"
-done
 
-out_bam={snakemake.output.bam}
+    touch \
+        {snakemake.output.bam} \
+        {snakemake.output.bam_md5} \
+        {snakemake.output.bam_bai} \
+        {snakemake.output.bam_bai_md5}
 
-if [[ $i == 2 ]]; then
-    mv $TMPDIR/sorted/1.bam $out_bam
-else
-    samtools merge -@ 8 $out_bam $TMPDIR/sorted/*.bam
-fi
+    # We use stdbuf on samtools index above toether with stdout redirection.  We have seen
+    # some interesting timing issues with clustered/distributed file systems and the small
+    # index file not having been written by the time that samtools idxstats uses it.  In
+    # addition we attempt to run it 5 times with incremental waits to make things more
+    # robust.
+    for i in {{1..5}}; do
+        set +e
+        samtools idxstats {snakemake.output.bam} >{snakemake.output.report_idxstats_txt}
+        ret=$?
+        set -e
+        if [[ "$ret" -eq 0 ]]; then
+            break
+        elif [[ "$i" -lt 5 ]]; then
+            sleep "${{i}}m"
+        fi
+    done
+    if [[ "$ret" -ne 0 ]]; then
+        >&2 echo "samtools idxstats failed 5 times - something wrong with the index?"
+        exit 1
+    fi
+    compute-md5 {snakemake.output.bam_bai} {snakemake.output.bam_bai_md5}
 
-cp $out_bam /tmp
-
-samtools index $out_bam
-
-# Compute MD5 sums
-pushd $(dirname $out_bam)
-md5sum $(basename $out_bam) >$(basename $out_bam).md5
-md5sum $(basename $out_bam).bai >$(basename $out_bam).bai.md5
-popd
-
-# QC Report ---------------------------------------------------------------------------------------
-
-# gather statistics from BAM file
-# TODO: use pipes for only reading once from disk?
-samtools stats    {snakemake.output.bam} > {snakemake.output.report_bamstats_txt}
-samtools flagstat {snakemake.output.bam} > {snakemake.output.report_flagstats_txt}
-samtools idxstats {snakemake.output.bam} > {snakemake.output.report_idxstats_txt}
-
-# Build MD5 files for the reports
-md5sum {snakemake.output.report_bamstats_txt} > {snakemake.output.report_bamstats_txt_md5}
-md5sum {snakemake.output.report_flagstats_txt} >{snakemake.output.report_flagstats_txt_md5}
-md5sum {snakemake.output.report_idxstats_txt} > {snakemake.output.report_idxstats_txt_md5}
+    compute-md5 {snakemake.output.report_bamstats_txt}  {snakemake.output.report_bamstats_txt_md5}
+    compute-md5 {snakemake.output.report_flagstats_txt} {snakemake.output.report_flagstats_txt_md5}
+    compute-md5 {snakemake.output.report_idxstats_txt}  {snakemake.output.report_idxstats_txt_md5}
+}}
 
 # Create output links -----------------------------------------------------------------------------
 
