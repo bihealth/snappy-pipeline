c8f0a7c1747672be02875d8013188d1510077510
Eric Blanc <eric.blanc@bihealth.de>
2023-10-05 18:14:30 +0200

    refactor: Allow input from snakemake or from config

--- snappy_wrappers/wrappers/purecn/coverage/wrapper.py
+++ snappy_wrappers/wrappers/purecn/coverage/wrapper.py
@@ -11,22 +11,45 @@ __author__ = "Eric Blanc <eric.blanc@bih-charite.de>"
 step = snakemake.config["pipeline_step"]["name"]
 config = snakemake.config["step_config"][step]["purecn"]
 
-shell.executable("/bin/bash")
+if "container" in snakemake.input.keys() and snakemake.input.container:
+    container = snakemake.input.container
+elif "path_container" in config.keys() and config["path_container"]:
+    container = config["path_container"]
+else:
+    raise Exception("No path to PureCN container")
+
+if "intervals" in snakemake.input.keys() and snakemake.input.intervals:
+    intervals = snakemake.input.intervals
+elif "path_intervals" in config.keys() and config["path_intervals"]:
+    intervals = config["path_intervals"]
+else:
+    raise Exception("No path to PureCN intervals")
 
 # Prepare files and directories that must be accessible by the container
+files_to_bind = {
+    "bam": snakemake.input.bam,
+}
+if "intervals" not in snakemake.input.keys():
+    files_to_bind["intervals"] = intervals
+
+# Replace with full absolute paths
+files_to_bind = {k: os.path.realpath(v) for k, v in files_to_bind.items()}
+# Directories that mut be bound
+dirs_to_bind = {k: os.path.dirname(v) for k, v in files_to_bind.items()}
+# List of unique directories to bind: on cluster: <directory> -> from container: /bindings/d<i>)
+bound_dirs = {e[1]: e[0] for e in enumerate(list(set(dirs_to_bind.values())))}
+# Binding command
+bindings = " ".join(["-B {}:/bindings/d{}:ro".format(k, v) for k, v in bound_dirs.items()])
+# Path to files from the container
 bound_files = {
-    "bam": os.path.realpath(snakemake.input.bam),
+    k: "/bindings/d{}/{}".format(bound_dirs[dirs_to_bind[k]], os.path.basename(v))
+    for k, v in files_to_bind.items()
 }
 
-keys = list(bound_files.keys())
-bindings = []
-for i in range(len(keys)):
-    k = keys[i]
-    if bound_files[k]:
-        # Binding directory to /bindings/d<i>
-        bindings.append(" -B {}:/bindings/d{}:ro".format(os.path.dirname(bound_files[k]), i))
-        bound_files[k] = "/bindings/d{}/{}".format(i, os.path.basename(bound_files[k]))
-bindings = " ".join(bindings)
+if "intervals" in bound_files.keys():
+    intervals = bound_files["intervals"]
+
+shell.executable("/bin/bash")
 
 shell(
     r"""
@@ -58,10 +81,10 @@ cmd="/usr/local/bin/Rscript /opt/PureCN/Coverage.R --force \
     --seed {config[seed]} \
     --out-dir $(dirname {snakemake.output.coverage}) \
     --bam {bound_files[bam]} \
-    --intervals {snakemake.input.intervals}
+    --intervals {intervals}
 "
 mkdir -p $(dirname {snakemake.output.coverage})
-apptainer exec --home $PWD {bindings} {snakemake.input.container} $cmd
+apptainer exec --home $PWD {bindings} {container} $cmd
 
 pushd $(dirname {snakemake.output.coverage})
 md5sum $(basename {snakemake.output.coverage}) > $(basename {snakemake.output.coverage}).md5
