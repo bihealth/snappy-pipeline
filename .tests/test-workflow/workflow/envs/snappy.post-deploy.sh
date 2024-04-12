0570201668abb684c36bd3228de81245059aafb8
Till Hartmann <till.hartmann@bih-charite.de>
2024-04-12 10:24:41 +0200

    fix typo

--- .tests/test-workflow/workflow/envs/snappy.post-deploy.sh
+++ .tests/test-workflow/workflow/envs/snappy.post-deploy.sh
@@ -2,8 +2,8 @@
 set -o pipefail
 conda init
 conda activate "$CONDA_PREFIX"
-if [[ -z "${$GITHUB_WORKSPACE}" ]]; then
-  cd "${$GITHUB_WORKSPACE}" || exit
+if [[ -z "${GITHUB_WORKSPACE}" ]]; then
+  cd "${GITHUB_WORKSPACE}" || exit
 else
   cd snappy-pipeline || exit
 fi
