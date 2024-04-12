fe582a77e18abeb6303a623676884b501927579a
Till Hartmann <till.hartmann@bih-charite.de>
2024-04-12 10:32:55 +0200

    try hardcoding github workspace

--- .tests/test-workflow/workflow/envs/snappy.post-deploy.sh
+++ .tests/test-workflow/workflow/envs/snappy.post-deploy.sh
@@ -2,9 +2,14 @@
 set -o pipefail
 conda init
 conda activate "$CONDA_PREFIX"
+
 if [[ -z "${GITHUB_WORKSPACE}" ]]; then
   cd "${GITHUB_WORKSPACE}" || exit
 else
-  cd snappy-pipeline || exit
+  if [[ -d /github/workspace ]; then
+    cd /github/workspace/ || exit
+  else
+    cd snappy-pipeline || exit
+  fi
 fi
 pip install -e .
