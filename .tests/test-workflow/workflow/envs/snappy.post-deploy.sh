c6d15d11bbd12642880d4c1fb77f00f0e55f8b87
Till Hartmann <till.hartmann@bih-charite.de>
2024-04-12 09:28:02 +0200

    use symlink instead of git checkout in snappy post-deploy script

--- .tests/test-workflow/workflow/envs/snappy.post-deploy.sh
+++ .tests/test-workflow/workflow/envs/snappy.post-deploy.sh
@@ -2,7 +2,5 @@
 set -o pipefail
 conda init
 conda activate "$CONDA_PREFIX"
-git clone https://github.com/bihealth/snappy-pipeline.git
 cd snappy-pipeline || exit
-git checkout e260347ebed0b54cc8c84e48c932581ac59cd667
 pip install -e .
