ca11b71460f9ff50da18d965c01b59e3e5630fc3
Till Hartmann <till.hartmann@bih-charite.de>
2024-05-23 14:33:49 +0200

    add pydantic model for somatic_targeted_seq_cnv_calling

---
+++ snappy_pipeline/workflows/somatic_targeted_seq_cnv_calling/model.py
@@ -0,0 +1,391 @@
+import enum
+from typing import Annotated, Any, Self
+
+from pydantic import Field, model_validator
+
+from models import SnappyStepModel, EnumField, SnappyModel
+
+
+class Tool(enum.Enum):
+    cnvkit = "cnvkit"
+    sequenza = "sequenza"
+    copywriter = "copywriter"
+    cnvetti_on_target = "cnvetti_on_target"
+    cnvetti_off_target = "cnvetti_off_target"
+    purecn = "purecn"
+
+
+class SegmentationMethod(enum.Enum):
+    cbs = "cbs"
+    flasso = "flasso"
+    haar = "haar"
+    hmm = "hmm"
+    hmm_tumor = "hmm-tumor"
+    hmm_germline = "hmm-germline"
+    none = "none"
+
+
+class CenterMode(enum.Enum):
+    mean = "mean"
+    median = "median"
+    mode = "mode"
+    biweight = "biweight"
+
+
+class FilterMode(enum.Enum):
+    ampdel = "ampdel"
+    cn = "cn"
+    ci = "ci"
+    sem = "sem"
+
+
+class CallingMethod(enum.Enum):
+    threshold = "threshold"
+    clonal = "clonal"
+    none = ""
+
+
+class Gender(enum.Enum):
+    male = "male"
+    female = "female"
+    guess = ""
+
+
+class Cnvkit(SnappyModel):
+    path_target: str
+    """Usually ../panel_of_normals/output/cnvkit.target/out/cnvkit.target.bed"""
+
+    path_antitarget: str
+    """Usually ../panel_of_normals/output/cnvkit.antitarget/out/cnvkit.antitarget.bed"""
+
+    path_panel_of_normals: str
+    """Usually ../panel_of_normals/output/{mapper}.cnvkit.create_panel/out/{mapper}.cnvkit.panel_of_normals.cnn"""
+
+    plot: bool = True
+    """Generate plots (very slow)"""
+
+    min_mapq: int = 0
+    """[coverage] Mininum mapping quality score to count a read for coverage depth"""
+
+    count: bool = False
+    """[coverage] Alternative counting algorithm"""
+
+    gc_correction: bool = True
+    """[fix] Use GC correction"""
+
+    edge_correction: bool = True
+    """[fix] Use edge correction"""
+
+    rmask_correction: bool = True
+    """[fix] Use rmask correction"""
+    # BCBIO uses
+    # seg_method: haar
+    # seg_threshold: 0.0001
+    # -- OR
+    # seg_method: cbs
+    # seg_threshold: 0.000001
+    segmentation_method: SegmentationMethod = SegmentationMethod.cbs
+    """[segment] One of cbs, flasso, haar, hmm, hmm-tumor, hmm-germline, none"""
+
+    segmentation_threshold: float = 0.000001
+    """[segment] Significance threshold (hmm methods: smoothing window size)"""
+
+    drop_low_coverage: bool = False
+    """[segment, call, genemetrics] Drop very low coverage bins"""
+
+    drop_outliers: 10
+    """[segment] Drop outlier bins (0 for no outlier filtering)"""
+
+    smooth_cbs: bool = True
+    """[segment] Additional smoothing of CBS segmentation (WARNING- not the default value)"""
+
+    center: CenterMode | float | None = None
+    """[call] Either one of mean, median, mode, biweight, or a constant log2 ratio value."""
+
+    filter: FilterMode | str = FilterMode.ampdel
+    """
+    [call] One of ampdel, cn, ci, sem (merging segments flagged with the specified filter),
+    "" for no filtering
+    """
+
+    calling_method: CallingMethod = CallingMethod.threshold
+    """[call] One of threshold, clonal, none"""
+
+    call_thresholds: str = "-1.1,-0.25,0.2,0.7"
+    """[call] Thresholds for calling integer copy number"""
+
+    ploidy: int = 2
+    """[call] Ploidy of sample cells"""
+    purity: Annotated[float, Field(0, ge=0, le=1)]
+    """[call] Estimated tumor cell fraction (0 for discarding tumor cell purity)"""
+
+    gender: Gender = Gender.guess
+    """
+    [call, diagram] Specify the chromosomal sex of all given samples as male or female.
+    Guess when missing
+    """
+
+    male_reference: bool = False
+    """[call, diagram] Create male reference"""
+    diagram_threshold: float = 0.5
+    """[diagram] Copy number change threshold to label genes"""
+
+    diagram_min_probes: int = 3
+    """[diagram] Min number of covered probes to label genes"""
+
+    shift_xy: bool = True
+    """[diagram] Shift X & Y chromosomes according to sample sex"""
+
+    breaks_min_probes: int = 1
+    """[breaks] Min number of covered probes for a break inside the gene"""
+
+    genemetrics_min_probes: int = 3
+    """[genemetrics] Min number of covered probes to consider a gene"""
+
+    genemetrics_threshold: float = 0.2
+    """[genemetrics] Min abs log2 change to consider a gene"""
+
+    genemetrics_alpha: float = 0.05
+    """[genemetrics] Significance cutoff"""
+
+    genemetrics_bootstrap: int = 100
+    """[genemetrics] Number of bootstraps"""
+
+    segmetrics_alpha: float = 0.05
+    """[segmetrics] Significance cutoff"""
+
+    segmetrics_bootstrap: int = 100
+    """[segmetrics] Number of bootstraps"""
+
+    smooth_bootstrap: bool = False
+    """[segmetrics] Smooth bootstrap results"""
+
+
+class SequenzaExtraArgs(SnappyModel):
+    hom: float = 0.9
+    """Threshold to select homozygous positions"""
+
+    het: float = 0.25
+    """Threshold to select heterozygous positions"""
+
+    qlimit: float = 20
+    """Minimum nucleotide quality score for inclusion in the counts"""
+
+    qformat: str = "sanger"
+    """Quality format, options are "sanger" or "illumina". This will add an offset of 33 or 64 respectively to the qlimit value"""
+
+
+class SequenzaExtractExtraArgs(SnappyModel):
+    gamma: int = 60
+    """scarHRD value"""
+
+    kmin: int = 50
+    """scarHRD value"""
+
+
+class SequenzaFitExtraArgs(SnappyModel):
+    N_ratio_filter: int = Field(10, alias="N.ratio.filter")
+    N_BAF_filter: int = Field(1, alias="N.BAF.filter")
+    segment_filter: int = Field(3000000, alias="segment.filter")
+    mufreq_treshold: float = Field(0.1, alias="mufreq.threshold")
+    ratio_priority: bool = Field(False, alias="ratio_priority")
+    ploidy: list[float] = [
+        1.0,
+        1.1,
+        1.2,
+        1.3,
+        1.4,
+        1.5,
+        1.6,
+        1.7,
+        1.8,
+        1.9,
+        2.0,
+        2.1,
+        2.2,
+        2.3,
+        2.4,
+        2.5,
+        2.6,
+        2.7,
+        2.8,
+        2.9,
+        3.0,
+        3.1,
+        3.2,
+        3.3,
+        3.4,
+        3.5,
+        3.6,
+        3.7,
+        3.8,
+        3.9,
+        4.0,
+        4.1,
+        4.2,
+        4.3,
+        4.4,
+        4.5,
+        4.6,
+        4.7,
+        4.8,
+        4.9,
+        5.0,
+        5.1,
+        5.2,
+        5.3,
+        5.4,
+        5.5,
+    ]
+
+
+class Sequenza(SnappyModel):
+    length: int = 50
+    assembly: str = "hg19"
+    """Must be hg38 for GRCh38. See copynumber for complete list (augmented with hg38)"""
+
+    extra_args: SequenzaExtraArgs | dict[str, Any] = {}
+    """Extra arguments for sequenza bam2seqz"""
+
+    ignore_chroms: list[str] = [
+        "X",
+        "Y",
+        "MT",
+        "NC_007605. hs37d5",
+        "chrEBV",
+        "*_decoy",
+        "HLA-*",
+        "GL000220.*",
+    ]
+    """patterns of chromosome names to ignore"""
+
+    extra_args_extract: SequenzaExtractExtraArgs | dict[str, Any] = SequenzaExtractExtraArgs()
+    """Valid arguments: see ?sequenza::sequenza.extract in R"""
+
+    extra_args_fit: SequenzaFitExtraArgs | dict[str, Any] = SequenzaFitExtraArgs()
+    """Valid arguments: see ?sequenza::sequenza.fit in R"""
+
+
+class CopyWriter(SnappyModel):
+    path_target_regions: str
+
+    bin_size: int = 20000  # TODO: make actually configurable
+
+    plot_genes: str
+    """Path to civic annotation"""
+
+    genome: str = "hg19"
+    """Could be hg38 (consider setting prefix to 'chr' when using GRCh38.v1)"""
+
+    features: str = "EnsDb.Hsapiens.v75::EnsDb.Hsapiens.v75"
+
+    prefix: str = ""
+
+    nThread: int = 8
+
+
+class GenomeName(enum.Enum):
+    hg18 = "hg18"
+    hg19 = "hg19"
+    hg38 = "hg38"
+    mm9 = "mm9"
+    mm10 = "mm10"
+    rn4 = "rn4"
+    rn5 = "rn5"
+    rn6 = "rn6"
+    canFam3 = "canFam3"
+
+
+class PureCnExtraCommands(SnappyModel):
+    model: str = "betabin"
+    fun_segmentation: str = Field("PSCBS", alias="fun-segmentation")
+    post_optimize: str = ""
+    """post-optimize is a flag"""
+
+
+class PureCn(SnappyModel):
+    genome_name: Annotated[GenomeName, EnumField(GenomeName)]
+    """Must be one from hg18, hg19, hg38, mm9, mm10, rn4, rn5, rn6, canFam3"""
+
+    enrichment_kit_name: str = "unknown"
+    """For filename only..."""
+
+    mappability: str = ""
+    """
+    GRCh38:
+     /fast/work/groups/cubi/projects/biotools/static_data/app_support/PureCN/hg38/mappability.bw
+    """
+
+    reptiming: str = ""
+    """Nothing for GRCh38"""
+
+    seed: int = 1234567
+    extra_commands: PureCnExtraCommands
+    """Recommended extra arguments for PureCN, extra_arguments: [] to clear them all"""
+
+    path_container: Annotated[
+        str, Field(examples=["../panel_of_normals/work/containers/out/purecn.simg"])
+    ]
+    """
+    A PureCN panel of normals is required,
+    with the container, the intervals & the PON rds file
+    """
+
+    path_intervals: Annotated[
+        str,
+        Field(
+            examples=[
+                "../panel_of_normals/output/purecn/out/<enrichement_kit_name>_<genome_name>.list"
+            ]
+        ),
+    ]
+
+    path_panel_of_normals: Annotated[
+        str,
+        Field(
+            examples=["../panel_of_normals/output/bwa.purecn/out/bwa.purecn.panel_of_normals.rds"]
+        ),
+    ]
+    path_mapping_bias: Annotated[
+        str,
+        Field(examples=["../panel_of_normals/output/bwa.purecn/out/bwa.purecn.mapping_bias.rds"]),
+    ]
+
+    somatic_variant_caller: str = "mutect2"
+    """
+    IMPORTANT NOTE:
+    Mutect2 must be called with "--genotype-germline-sites true --genotype-pon-sites true
+    """
+
+    path_somatic_variants: Annotated[
+        str, Field(examples=["../ somatic_variant_calling_for_purecn"])
+    ]
+
+
+class CnvettiOnTarget(SnappyModel):
+    path_target_regions: str
+
+
+class CnvettiOffTarget(SnappyModel):
+    path_target_regions: str
+
+    window_length: int = 20000
+
+
+class SomaticTargetedSeqCnvCalling(SnappyStepModel):
+    tools: Annotated[list[Tool], EnumField(Tool, [Tool.cnvkit])]
+    path_ngs_mapping: str
+
+    cnvkit: Cnvkit | None = None
+    sequenza: Sequenza | None = None
+    copywriter: CopyWriter | None = None
+    purecn: PureCn | None = None
+    cnvetti_on_target: CnvettiOnTarget | None = None
+    cnvetti_off_target: CnvettiOffTarget | None = None
+
+    @model_validator(mode="after")
+    def ensure_tools_are_configured(self: Self) -> Self:
+        for tool in self.tools:
+            if not getattr(self, str(tool)):
+                raise ValueError(f"Tool {tool} not configured")
+        return self
