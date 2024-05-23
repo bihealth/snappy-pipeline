ad5ffe559e402b8a2c00f376bbc8f8b87caa6e0f
Till Hartmann <till.hartmann@bih-charite.de>
2024-05-23 13:46:50 +0200

    add pydantic model for somatic_cnv_checking

---
+++ snappy_pipeline/workflows/somatic_cnv_checking/model.py
@@ -0,0 +1,36 @@
+import enum
+from typing import Annotated
+
+from pydantic import Field
+
+from models import SnappyStepModel
+
+
+class CnvAssayType(enum.Enum):
+    WES = "WES"
+    WGS = "WGS"
+
+
+class SomaticCnvChecking(SnappyStepModel):
+    path_ngs_mapping: str
+
+    path_cnv_calling: Annotated[str, Field(examples=["../somatic_targeted_seq_cnv_calling"])] = ""
+
+    cnv_assay_type: CnvAssayType | None = None
+    """
+    Empty: no CNV,
+    WES for somatic_targeted_seq_snv_calling step,
+    WGS for somatic_wgs_cnv_calling step
+    """
+
+    excluded_regions: str = ""
+    """Bed file of regions to be excluded"""
+
+    max_depth: int = 10000
+    """Max depth for pileups"""
+
+    min_depth: int = 20
+    """Minimum depth for reference and alternative alleles to consider variant"""
+
+    min_baf: Annotated[float, Field(0.4, ge=0, le=0.5)]
+    """Maximum BAF to consider variant as heterozygous (between 0 & 1/2)"""
