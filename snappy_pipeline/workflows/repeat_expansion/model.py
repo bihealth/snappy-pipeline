7fd51228976cfe58c46feade9bcc7f14f7d6fa5a
Till Hartmann <till.hartmann@bih-charite.de>
2024-05-23 13:34:27 +0200

    add pydantic model for repeat_expansion

---
+++ snappy_pipeline/workflows/repeat_expansion/model.py
@@ -0,0 +1,12 @@
+from models import SnappyStepModel
+
+
+class RepeatExpansion(SnappyStepModel):
+    repeat_catalog: str
+    """Repeat expansions definitions - used in ExpansionHunter call"""
+
+    repeat_annotation: str
+    """Repeat expansions annotations, e.g., normality range - custom file"""
+
+    path_ngs_mapping: str
+    """Path to the ngs_mapping step"""
