9fccc36d6158ba28c2d840d80ebf66a1d0f2980b
Till Hartmann <till.hartmann@bih-charite.de>
2024-05-23 11:16:26 +0200

    add pydantic model for helper_gcnv_model_wgs

---
+++ snappy_pipeline/workflows/helper_gcnv_model_wgs/model.py
@@ -0,0 +1,12 @@
+from models import SnappyStepModel, SnappyModel
+
+
+class Gcnv(SnappyModel):
+    path_uniquely_mapable_bed: str
+    """path to BED file with uniquely mappable regions."""
+
+
+class HelperGcnvModelWgs(SnappyStepModel):
+    path_ngs_mapping: str
+
+    gcnv: Gcnv
