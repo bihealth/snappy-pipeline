7b8f2c15ead5ec44ff9cce09bacd456c58fa00c3
Till Hartmann <till.hartmann@bih-charite.de>
2024-05-23 11:31:56 +0200

    add pydantic model for igv_session_generation

---
+++ snappy_pipeline/workflows/igv_session_generation/model.py
@@ -0,0 +1,30 @@
+from typing import Self
+
+from pydantic import model_validator
+
+from models import SnappyStepModel
+
+
+class IgvSessionGeneration(SnappyStepModel):
+    path_ngs_mapping: str
+
+    path_variant_phasing: str = ""
+
+    path_variant_annotation: str = ""
+
+    path_variant_calling: str = ""
+
+    tools_ngs_mapping: list[str] = []
+    """defaults to ngs_mapping tool"""
+
+    tools_variant_calling: list[str] = []
+    """defaults to variant_annotation tool"""
+
+    @model_validator(mode="after")
+    def ensure_at_least_one_path_is_specified(self: Self) -> Self:
+        if not any(
+            getattr(self, path)
+            for path in ("path_variant_phasing", "path_variant_annotation", "path_variant_calling")
+        ):
+            raise ValueError(f"No path specified for variant phasing, annotation or calling")
+        return self
