8e0f54ca08e3db096df95062ac9d6a4ae329289f
Till Hartmann <till.hartmann@bih-charite.de>
2024-05-23 14:01:15 +0200

    add pydantic model for somatic_purity_ploidy_estimate

---
+++ snappy_pipeline/workflows/somatic_purity_ploidy_estimate/model.py
@@ -0,0 +1,39 @@
+import enum
+from typing import Annotated, Self
+
+from pydantic import model_validator
+
+from models import SnappyStepModel, EnumField, SnappyModel
+
+
+class Tool(enum.Enum):
+    ascat = "ascat"
+
+
+class Ascat(SnappyModel):
+    b_af_loci: str
+    """BED file with loci for B allele frequency."""
+
+
+class SomaticPurityPloidyEstimate(SnappyStepModel):
+    tools: Annotated[list[Tool], EnumField(Tool, [Tool.ascat])]
+
+    tool_cnv_calling: str = "cnvetti"
+
+    tool_ngs_mapping: str = "bwa"
+    """
+        Configuration with read mapper and path to mapping output.
+        Will use this for generating a pileup using samtools
+        for obtaining the b allele fraction and computing coverage.
+    """
+
+    path_ngs_mapping: str
+
+    ascat: Ascat | None = None
+
+    @model_validator(mode="after")
+    def ensure_tools_are_configured(self: Self) -> Self:
+        for tool in self.tools:
+            if not getattr(self, str(tool)):
+                raise ValueError(f"Tool {tool} not configured")
+        return self
