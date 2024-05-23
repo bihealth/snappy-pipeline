8d64ebbd0402b319ac7c3bc57aab14ffb2d9e2aa
Till Hartmann <till.hartmann@bih-charite.de>
2024-05-23 11:23:54 +0200

    add pydantic model for hla_typing

---
+++ snappy_pipeline/workflows/hla_typing/model.py
@@ -0,0 +1,42 @@
+import enum
+from typing import Annotated, Self
+
+from pydantic import model_validator
+
+from models import SnappyStepModel, EnumField, SnappyModel
+
+
+class Tool(enum.Enum):
+    optitype = "optitype"
+    arcashla = "arcashla"
+
+
+class Optitype(SnappyModel):
+    max_reads: int = 5000
+    """5000 is a suggestion by OptiType author"""
+
+    num_mapping_threads: int = 4
+
+
+class ArcasHla(SnappyModel):
+    mapper: str = "star"
+
+
+class HlaTyping(SnappyStepModel):
+    path_ngs_mapping: str
+
+    path_link_in: str = ""
+    """Override data set configuration search paths for FASTQ files"""
+
+    tools: Annotated[list[Tool], EnumField(Tool, [Tool.optitype])]
+
+    optitype: Optitype | None = None
+
+    arcashla: ArcasHla | None = None
+
+    @model_validator(mode="after")
+    def ensure_tools_are_configured(self: Self) -> Self:
+        for tool in self.tools:
+            if not getattr(self, str(tool)):
+                raise ValueError(f"Tool {tool} not configured")
+        return self
