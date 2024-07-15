d43a11b69f3af7378401a28bb390014cb6f6969a
giacuong171 <giacuong171@gmail.com>
2024-07-15 14:02:15 +0200

    Adding test for neoepitope prediction

--- tests/snappy_pipeline/workflows/test_workflows_somatic_neoepitope_prediction.py
+++ tests/snappy_pipeline/workflows/test_workflows_somatic_neoepitope_prediction.py
@@ -5,6 +5,7 @@ import textwrap
 
 import pytest
 import ruamel.yaml as ruamel_yaml
+from snakemake.io import Wildcards
 
 from snappy_pipeline.workflows.somatic_neoepitope_prediction import (
     SomaticNeoepitopePredictionWorkflow,
@@ -50,19 +51,38 @@ def minimal_config():
             scalpel:
               path_target_regions: /path/to/target/regions.bed
 
+          hla_typing:
+            path_link_in: ''  # OPTIONAL Override data set configuration search paths for FASTQ files
+            tools: [optitype]   # REQUIRED - available: 'optitype' and 'arcashla'
+            optitype:
+                max_reads: 5000
+                num_mapping_threads: 4
+
           somatic_variant_annotation:
             path_somatic_variant_calling: ../somatic_variant_calling
             tools: ["vep"]
             vep:
-              cache_dir: /path/to/dir/cache
-
+                cache_dir: /path/to/dir/cache
+                species: homo_sapiens_merged
+                assembly: GRCh38
+                cache_version: '102'
+                tx_flag: gencode_basic  
+                num_threads: 8
+                buffer_size: 500
+                output_options:
+                - everything
           somatic_neoepitope_prediction:
-            path_somatic_variant_annotation: ../somatic_variant_annotation
+            path_somatic_variant_annotation: ../somatic_variant_annotation  # REQUIRED
+            path_container: ../somatic_neoepitope_prediction/work/containers/out/pvactools.simg
             path_rna_ngs_mapping: ../ngs_mapping
-            tools_somatic_variant_annotation: ["vep"]
+            path_hla_typing: ../hla_typing
+            tools_somatic_variant_annotation: [vep]
             preparation:
-                format: 'snappy_custom'
-                mode: 'gene'
+                format: snappy_custom    # REQUIRED - The file format of the expression file to process. (stringtie, kallisto, cufflinks, snappy_custom, custom)
+                mode: gene   # REQUIRED - Determine whether the expression file contains gene or transcript TPM values.
+            prediction:
+                algorithms: ['MHCflurry','MHCnuggetsI']
+                
         data_sets:
           first_batch:
             file: sheet.tsv
@@ -96,6 +116,7 @@ def somatic_neoepitope_prediction_workflow(
     dummy_workflow.globals = {
         "ngs_mapping": lambda x: "NGS_MAPPING/" + x,
         "somatic_variant_annotation": lambda x: "SOMATIC_VARIANT_ANNOTATION/" + x,
+        "hla_typing": lambda x: "HLA_TYPING/" + x,
     }
     # Construct the workflow object
     return SomaticNeoepitopePredictionWorkflow(
@@ -107,58 +128,146 @@ def somatic_neoepitope_prediction_workflow(
     )
 
 
-def test_neoepitope_preparation_step_part_get_input_files(somatic_neoepitope_prediction_workflow):
+def test_somatic_neoepitope_preparation_step_part_get_input_files(
+    somatic_neoepitope_prediction_workflow,
+):
+    wildcards = Wildcards(
+        fromdict={
+            "mapper": "bwa",
+            "var_caller": "mutect2",
+            "anno_caller": "vep",
+            "tumor_library": "P001-T1-DNA1-WGS1",
+        }
+    )
     # Define expected
     vcf_base_out = (
         "SOMATIC_VARIANT_ANNOTATION/output/{mapper}.{var_caller}.{anno_caller}.{tumor_library}/out/"
         "{mapper}.{var_caller}.{anno_caller}.{tumor_library}"
     )
-    ngs_mapping_base_out = "NGS_MAPPING/output/star.P002-T2-RNA1-mRNA_seq1/out/"
+    # vcf_base_out = (
+    #     "SOMATIC_VARIANT_ANNOTATION/output/bwa.mutect2.vep.P001-T1-DNA1-WES1/out/"
+    #     "{mapper}.{var_caller}.{anno_caller}.{tumor_library}"
+    # )
+    ngs_mapping_base_out = "NGS_MAPPING/output/star.P001-T1-RNA1-mRNA_seq1/out/"
     expected = {
         "vcf": vcf_base_out + ".full.vcf.gz",
         "vcf_tbi": vcf_base_out + ".full.vcf.gz.tbi",
-        "expression": ngs_mapping_base_out + "star.P002-T2-RNA1-mRNA_seq1.GeneCounts.tab",
-        "bam": ngs_mapping_base_out + "star.P002-T2-RNA1-mRNA_seq1.bam",
-        "bai": ngs_mapping_base_out + "star.P002-T2-RNA1-mRNA_seq1.bam.bai",
+        "expression": ngs_mapping_base_out + "star.P001-T1-RNA1-mRNA_seq1.GeneCounts.tab",
+        "bam": ngs_mapping_base_out + "star.P001-T1-RNA1-mRNA_seq1.bam",
     }
 
     # Get actual
-    actual = somatic_neoepitope_prediction_workflow.get_input_files("neoepitope_preparation", "run")
+    actual = somatic_neoepitope_prediction_workflow.get_input_files("pvacseq", "prepare")(wildcards)
     assert actual == expected
 
 
-def test_neoepitope_preparation_step_part_get_output_files(somatic_neoepitope_prediction_workflow):
-    base_out = (
-        "work/{mapper}.{var_caller}.{anno_caller}.GX.{tumor_library}/out/"
-        "{mapper}.{var_caller}.{anno_caller}.GX.{tumor_library}"
-    )
-    expected = get_expected_output_vcf_files_dict(base_out)
-    actual = somatic_neoepitope_prediction_workflow.get_output_files(
-        "neoepitope_preparation", "run"
-    )
-    assert actual == expected
-
-
-def test_neoepitope_preparation_step_part_get_log_files(somatic_neoepitope_prediction_workflow):
-    base_out = (
-        "work/{mapper}.{var_caller}.{anno_caller}.GX.{tumor_library}/log/"
-        "{mapper}.{var_caller}.{anno_caller}.GX.{tumor_library}"
-    )
-    expected = get_expected_log_files_dict(base_out=base_out)
-    actual = somatic_neoepitope_prediction_workflow.get_log_file("neoepitope_preparation", "run")
-    assert actual == expected
-
-
-def test_neoepitope_preparation_step_part_get_resource(
+def test_somatic_neoepitope_prediction_step_part_get_input_files(
     somatic_neoepitope_prediction_workflow,
 ):
     # Define expected
-    expected_dict = {"threads": 2, "time": "1:00:00", "memory": "6144M"}
+    wildcards = Wildcards(
+        fromdict={
+            "mapper": "bwa",
+            "var_caller": "mutect2",
+            "anno_caller": "vep",
+            "tumor_library": "P001-T1-DNA1-WGS1",
+        }
+    )
+    vcf_base_out = (
+        "work/prepare/{mapper}.{var_caller}.{anno_caller}.{mode}.{tumor_library}/out/"
+        "{mapper}.{var_caller}.{anno_caller}.{mode}.{tumor_library}"
+    )
+    hla_typing_base_out = "HLA_TYPING/output/"
+    expected = {
+        "container": "work/containers/out/pvactools.simg",
+        "combine_vcf": vcf_base_out + ".vcf.gz",
+        "hla_normal_dna": hla_typing_base_out
+        + "optitype.P001-N1-DNA1-WGS1/out/optitype.P001-N1-DNA1-WGS1.txt",
+        "hla_tumor_dna": hla_typing_base_out
+        + "optitype.P001-T1-DNA1-WGS1/out/optitype.P001-T1-DNA1-WGS1.txt",
+        "hla_tumor_rna": hla_typing_base_out
+        + "optitype.P001-T1-RNA1-mRNA_seq1/out/optitype.P001-T1-RNA1-mRNA_seq1.txt",
+    }
+
+    # Get actual
+    actual = somatic_neoepitope_prediction_workflow.get_input_files("pvacseq", "predict")(wildcards)
+    assert actual == expected
+
+
+def test_somatic_neoepitope_preparation_step_part_get_output_files(
+    somatic_neoepitope_prediction_workflow,
+):
+    base_out = (
+        "work/prepare/{mapper}.{var_caller}.{anno_caller}.GX.{tumor_library}/out/"
+        "{mapper}.{var_caller}.{anno_caller}.GX.{tumor_library}"
+    )
+    expected = get_expected_output_vcf_files_dict(base_out)
+    actual = somatic_neoepitope_prediction_workflow.get_output_files("pvacseq", "prepare")
+    assert actual == expected
+
+
+def test_somatic_neoepitope_preparation_step_part_get_log_files(
+    somatic_neoepitope_prediction_workflow,
+):
+    base_out = (
+        "work/prepare/{mapper}.{var_caller}.{anno_caller}.GX.{tumor_library}/log/"
+        "{mapper}.{var_caller}.{anno_caller}.GX.{tumor_library}"
+    )
+    expected = get_expected_log_files_dict(base_out=base_out)
+    actual = somatic_neoepitope_prediction_workflow.get_log_file("pvacseq", "prepare")
+    assert actual == expected
+
+
+def test_somatic_neoepitope_preparation_step_part_get_resource(
+    somatic_neoepitope_prediction_workflow,
+):
+    # Define expected
+    expected_dict = {"threads": 1, "time": "01:00:00", "memory": "6G"}
     # Evaluate
     for resource, expected in expected_dict.items():
         msg_error = f"Assertion error for resource '{resource}'."
         actual = somatic_neoepitope_prediction_workflow.get_resource(
-            "neoepitope_preparation", "run", resource
+            "pvacseq", "prepare", resource
+        )()
+        assert actual == expected, msg_error
+
+
+def test_somatic_neoepitope_prediction_step_part_get_output_files(
+    somatic_neoepitope_prediction_workflow,
+):
+    base_out = "work/predict/{mapper}.{var_caller}.{anno_caller}.{mode}.{tumor_library}.epitopes/out/MHC_Class_I/{tumor_library}"
+    expected = {
+        "all_epitopes": base_out + ".all_epitopes.tsv",
+        "all_epitopes_md5": base_out + ".all_epitopes.tsv.md5",
+        "filtered_epitopes": base_out + ".filtered.tsv",
+        "filtered_epitopes_md5": base_out + ".filtered.tsv.md5",
+    }
+    actual = somatic_neoepitope_prediction_workflow.get_output_files("pvacseq", "predict")
+    assert actual == expected
+
+
+def test_somatic_neoepitope_prediction_step_part_get_log_files(
+    somatic_neoepitope_prediction_workflow,
+):
+    base_out = (
+        "work/predict/{mapper}.{var_caller}.{anno_caller}.{mode}.{tumor_library}.epitopes/"
+        "log/{tumor_library}"
+    )
+    expected = get_expected_log_files_dict(base_out=base_out)
+    actual = somatic_neoepitope_prediction_workflow.get_log_file("pvacseq", "predict")
+    assert actual == expected
+
+
+def test_somatic_neoepitope_prediction_step_part_get_resource(
+    somatic_neoepitope_prediction_workflow,
+):
+    # Define expected
+    expected_dict = {"threads": 4, "time": "4:00:00", "memory": "30G"}
+    # Evaluate
+    for resource, expected in expected_dict.items():
+        msg_error = f"Assertion error for resource '{resource}'."
+        actual = somatic_neoepitope_prediction_workflow.get_resource(
+            "pvacseq", "predict", resource
         )()
         assert actual == expected, msg_error
 
@@ -166,54 +275,47 @@ def test_neoepitope_preparation_step_part_get_resource(
 def test_somatic_neoepitope_prediction_workflow(somatic_neoepitope_prediction_workflow):
     """Test simple functionality of the workflow"""
     # Check created sub steps
-    expected = ["link_out", "neoepitope_preparation"]
+    expected = ["link_out", "pvacseq"]
     actual = list(sorted(somatic_neoepitope_prediction_workflow.sub_steps.keys()))
     assert actual == expected
 
     # Check result file construction
     tpl = (
-        "output/{mapper}.{var_caller}.{anno_caller}.GX.P00{i}-T{t}-DNA1-WGS1/{dir_}/"
-        "{mapper}.{var_caller}.{anno_caller}.GX.P00{i}-T{t}-DNA1-WGS1.{ext}"
+        "output/predict/{mapper}.{var_caller}.{anno_caller}.{mode}.P00{i}-T{t}-DNA1-WGS1.epitopes/"
+        "out/MHC_Class_I/P00{i}-T{t}-DNA1-WGS1{ext}"
+    )
+    log_tpl = (
+        "output/predict/{mapper}.{var_caller}.{anno_caller}.{mode}.P00{i}-T{t}-DNA1-WGS1.epitopes/"
+        "log/P00{i}-T{t}-DNA1-WGS1{ext}"
     )
     expected = [
-        tpl.format(
-            mapper=mapper,
-            var_caller=var_caller,
-            anno_caller=anno_caller,
-            i=i,
-            t=t,
-            ext=ext,
-            dir_="out",
+        log_tpl.format(
+            mapper="bwa", var_caller=var_caller, anno_caller="vep", i=i, t=t, ext=ext, mode="GX"
         )
-        for i, t in ((1, 1), (2, 2))
-        for ext in ("vcf.gz", "vcf.gz.tbi", "vcf.gz.md5", "vcf.gz.tbi.md5")
-        for mapper in ("bwa",)
+        for (i, t) in ((1, 1), (2, 2))
         for var_caller in ("mutect2", "scalpel")
-        for anno_caller in ("vep",)
+        for ext in (
+            ".log",
+            ".log.md5",
+            ".conda_info.txt",
+            ".conda_info.txt.md5",
+            ".conda_list.txt",
+            ".conda_list.txt.md5",
+        )
     ]
 
     expected += [
         tpl.format(
-            mapper=mapper,
-            var_caller=var_caller,
-            anno_caller=anno_caller,
-            i=i,
-            t=t,
-            ext=ext,
-            dir_="log",
+            mapper="bwa", var_caller=var_caller, anno_caller="vep", i=i, t=t, ext=ext, mode="GX"
         )
-        for i, t in ((1, 1), (2, 2))
-        for ext in (
-            "conda_info.txt",
-            "conda_list.txt",
-            "log",
-            "conda_info.txt.md5",
-            "conda_list.txt.md5",
-            "log.md5",
-        )
-        for mapper in ("bwa",)
+        for (i, t) in ((1, 1), (2, 2))
         for var_caller in ("mutect2", "scalpel")
-        for anno_caller in ("vep",)
+        for ext in (
+            ".all_epitopes.tsv",
+            ".filtered.tsv",
+            ".all_epitopes.tsv.md5",
+            ".filtered.tsv.md5",
+        )
     ]
     expected = list(sorted(expected))
     actual = list(sorted(somatic_neoepitope_prediction_workflow.get_result_files()))
