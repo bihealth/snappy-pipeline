59d5f32faddcdf3f0de68a21b5cc98a20550067e
Till Hartmann <till.hartmann@bih-charite.de>
2024-08-02 17:28:10 +0200

    chore: baf_file_generation: move config access to params

--- snappy_pipeline/workflows/variant_calling/__init__.py
+++ snappy_pipeline/workflows/variant_calling/__init__.py
@@ -870,6 +870,15 @@ class BafFileGenerationStepPart(GetResultFilesMixin, ReportGetLogFileMixin, Base
             ],
         )
 
+    def get_args(self, action: str):
+        def args_function(_wildcards):
+            return {
+                "min_dp": self.config.baf_file_generation.min_dp,
+                "reference_index_path": self.w_config.static_data_config.reference.path + ".fai",
+            }
+
+        return args_function
+
     def get_resource_usage(self, action: str, **kwargs) -> ResourceUsage:
         self._validate_action(action)
         return ResourceUsage(
