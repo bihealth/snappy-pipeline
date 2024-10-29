4b294434a37f2734b927ac59f108da1e1b85ac09
Till Hartmann <till.hartmann@bih-charite.de>
2024-10-29 18:43:53 +0100

    parse_args, not parse_known_args

--- snappy_pipeline/scripts/gcnv/contig_ploidy.py
+++ snappy_pipeline/scripts/gcnv/contig_ploidy.py
@@ -141,5 +141,5 @@ if __name__ == "__main__":
     parser.add_argument(
         "--output-done", required=True, help="Path to .done output marker"
     )
-    args = parser.parse_known_args()
+    args = parser.parse_args()
     main(args)
