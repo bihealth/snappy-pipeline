25e73d4c79846a8f9150bdb59cd36d508900f0a9
Eric Blanc <eric.blanc@bihealth.de>
2024-11-04 11:54:27 +0100

    refactor: use abstract cnvkit wrapper

--- snappy_wrappers/wrappers/cnvkit/fix/wrapper.py
+++ snappy_wrappers/wrappers/cnvkit/fix/wrapper.py
@@ -1,68 +1,24 @@
 # -*- coding: utf-8 -*-
 """Wrapper for cnvkit.py fix"""
 
-from snakemake.shell import shell
+from snappy_wrappers.wrappers.cnvkit.cnvkit_wrapper import CnvkitWrapper
 
-__author__ = "Manuel Holtgrewe"
-__email__ = "manuel.holtgrewe@bih-charite.de"
+__author__ = "Eric Blanc"
+__email__ = "eric.blanc@bih-charite.de"
 
-step = snakemake.config["pipeline_step"]["name"]
-config = snakemake.config["step_config"][step]["cnvkit"]
-
-if "ref" in snakemake.input.keys():
-    ref = snakemake.input.target
-elif "path_panel_of_normals" in config.keys():
-    ref = config["path_panel_of_normals"]
-else:
-    raise Exception("Unsupported naming")
-
-gender = " --gender {}".format(config["gender"]) if config["gender"] else ""
-male = " --male-reference" if config["male_reference"] else ""
-no_gc = " --no-gc" if not config["gc_correction"] else ""
-no_edge = " --no-edge" if not config["edge_correction"] else ""
-no_rmask = " --no-rmask" if not config["rmask_correction"] else ""
-
-shell(
-    r"""
-# Also pipe everything to log file
-if [[ -n "{snakemake.log.log}" ]]; then
-    if [[ "$(set +e; tty; set -e)" != "" ]]; then
-        rm -f "{snakemake.log.log}" && mkdir -p $(dirname {snakemake.log.log})
-        exec &> >(tee -a "{snakemake.log.log}" >&2)
-    else
-        rm -f "{snakemake.log.log}" && mkdir -p $(dirname {snakemake.log.log})
-        echo "No tty, logging disabled" >"{snakemake.log.log}"
-    fi
-fi
-
-# Write out information about conda installation.
-conda list >{snakemake.log.conda_list}
-conda info >{snakemake.log.conda_info}
-md5sum {snakemake.log.conda_list} >{snakemake.log.conda_list_md5}
-md5sum {snakemake.log.conda_info} >{snakemake.log.conda_info_md5}
-
-set -x
-
-# -----------------------------------------------------------------------------
-
+cmd = r"""
 cnvkit.py fix \
-    --output {snakemake.output.ratios} \
-    {gender} {male} {no_gc} {no_edge} {no_rmask} \
-    {snakemake.input.target} \
-    {snakemake.input.antitarget} \
-    {ref}
-
-d=$(dirname "{snakemake.output.ratios}")
-pushd $d
-fn=$(basename "{snakemake.output.ratios}")
-md5sum $fn > $fn.md5
-popd
-"""
+    -o {snakemake.output.coverage} \
+    {cluster} {snakemake.params.sample_id} \
+    {no_gc} {no_edge} {no_rmask} \
+    {snakemake.input.target} {antitarget} {snakemake.input.reference}
+""".format(
+    snakemake=snakemake,
+    cluster="--cluster" if snakemake.params.cluster else "",
+    no_gc="--no-gc" if snakemake.params.no_gc else "",
+    no_edge="--no-edge" if snakemake.params.no_edge else "",
+    no_rmask="--no-rmask" if snakemake.params.no_rmask else "",
+    antitarget=f"{snakemake.input.antitarget}" if "antitarget" in snakemake.input else "",
 )
 
-# Compute MD5 sums of logs.
-shell(
-    r"""
-md5sum {snakemake.log.log} >{snakemake.log.log_md5}
-"""
-)
+CnvkitWrapper(snakemake, cmd).run()
--- snappy_wrappers/wrappers/cnvkit/segment/wrapper.py
+++ snappy_wrappers/wrappers/cnvkit/segment/wrapper.py
@@ -1,67 +1,31 @@
 # -*- coding: utf-8 -*-
 """Wrapper vor cnvkit.py segment"""
 
-from snakemake.shell import shell
+from snappy_wrappers.wrappers.cnvkit.cnvkit_wrapper import CnvkitWrapper
 
-__author__ = "Manuel Holtgrewe"
-__email__ = "manuel.holtgrewe@bih-charite.de"
-
-step = snakemake.config["pipeline_step"]["name"]
-config = snakemake.config["step_config"][step]["cnvkit"]
-
-method = config["segmentation_method"]
-if method == "cbs" and config["smooth_cbs"]:
-    method += " --smooth-cbs"
-
-if float(config["segmentation_threshold"]) > 0:
-    threshold = " --threshold " + str(config["segmentation_threshold"])
+if "variants" in snakemake.input:
+    variants = r"""
+        ---vcf {snakemake.input.variants} \
+        {snakemake.params.sample_id} {snakemake.params.normal_id} \
+        {snakemake.params.min_variant_depth} {snakemake.params.zygocity_freq}
+    """.format(
+        snakemake=snakemake,
+    )
 else:
-    threshold = ""
+    variants = ""
 
-shell(
-    r"""
-# Also pipe everything to log file
-if [[ -n "{snakemake.log.log}" ]]; then
-    if [[ "$(set +e; tty; set -e)" != "" ]]; then
-        rm -f "{snakemake.log.log}" && mkdir -p $(dirname {snakemake.log.log})
-        exec &> >(tee -a "{snakemake.log.log}" >&2)
-    else
-        rm -f "{snakemake.log.log}" && mkdir -p $(dirname {snakemake.log.log})
-        echo "No tty, logging disabled" >"{snakemake.log.log}"
-    fi
-fi
-
-# Write out information about conda installation.
-conda list >{snakemake.log.conda_list}
-conda info >{snakemake.log.conda_info}
-md5sum {snakemake.log.conda_list} >{snakemake.log.conda_list_md5}
-md5sum {snakemake.log.conda_info} >{snakemake.log.conda_info_md5}
-
-set -x
-
-# -----------------------------------------------------------------------------
-
-cnvkit.py segment \
-    --output {snakemake.output.segments} \
-    --method {method} \
-    $(if [[ "{config[drop_low_coverage]}" = "True" ]]; then \
-        echo --drop-low-coverage
-    fi) \
-    {threshold} \
-    --drop-outliers {config[drop_outliers]} \
-    {snakemake.input}
-
-d=$(dirname "{snakemake.output.segments}")
-pushd $d
-fn=$(basename "{snakemake.output.segments}")
-md5sum $fn > $fn.md5
-popd
-"""
+cmd = r"""
+cnvkit.py segment --processes {snakemake.params.proceses} \
+    -o {snakemake.output.segments} --dataframe {snakemake.output.dataframe} \
+    --method {snakemake.params.method} --threshold {snakemake.params.threshold} {smooth_cbs} \
+    {drop_low_coverage} --drop-outliers {snakemake.params.drop_outliers} \
+    {variants} \
+    {snakemake.input.coverage}
+""".format(
+    snakemake=snakemake,
+    smooth_cbs="--smooth-cbs" if snakemake.params.smooth_cbs else "",
+    drop_low_coverage="--drop-low-coverage" if snakemake.params.drop_low_coverage else "",
+    variants=variants,
 )
 
-# Compute MD5 sums of logs.
-shell(
-    r"""
-md5sum {snakemake.log.log} >{snakemake.log.log_md5}
-"""
-)
+CnvkitWrapper(snakemake, cmd).run()
