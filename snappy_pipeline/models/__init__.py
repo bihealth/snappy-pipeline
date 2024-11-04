4534cc8bcea2551c238ee7b2d3370107a352b6aa
Eric Blanc <eric.blanc@bihealth.de>
2024-11-04 12:00:24 +0100

    refactor: parallel wrapper configuration model

--- snappy_pipeline/models/__init__.py
+++ snappy_pipeline/models/__init__.py
@@ -79,6 +79,46 @@ class SnappyModel(BaseModel):
         return self.model_fields.keys()
 
 
+class Parallel(SnappyModel):
+    num_cores: int = 2
+    """number of cores to use locally"""
+
+    window_length: int = 3500000
+    """split input into windows of this size, each triggers a job"""
+
+    num_jobs: int = 500
+    """number of windows to process in parallel"""
+
+    use_profile: bool = True
+    """use Snakemake profile for parallel processing"""
+
+    restart_times: int = 5
+    """number of times to re-launch jobs in case of failure"""
+
+    max_jobs_per_second: int = 2
+    """throttling of job creation"""
+
+    max_status_checks_per_second: int = 10
+    """throttling of status checks"""
+
+    debug_trunc_tokens: int = 0
+    """truncation to first N tokens (0 for none)"""
+
+    keep_tmpdir: KeepTmpdir = KeepTmpdir.never
+    """keep temporary directory, {always, never, onerror}"""
+
+    job_mult_memory: float = 1
+    """memory multiplier"""
+
+    job_mult_time: float = 1
+    """running time multiplier"""
+
+    merge_mult_memory: float = 1
+    """memory multiplier for merging"""
+
+    merge_mult_time: float = 1
+
+
 # This exists to distinguish workflow step_config models from other snappy specific models
 # It also provides a default_config_yaml_string method that includes the step_config section
 # by default.
