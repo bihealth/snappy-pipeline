6689ceb2808bd4ea5028a5aa217b9f4f24ea76a1
Eric Blanc <eric.blanc@bihealth.de>
2024-11-06 14:41:36 +0100

    fix: allow python 3.12 syntax in models to co-exist with pre-3.12 syntax in wrappers

--- snappy_wrappers/__init__.py
+++ snappy_wrappers/__init__.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 
-from snappy_pipeline.version import __version__
+# from snappy_pipeline.version import __version__
 
 __author__ = """Manuel Holtgrewe"""
 __email__ = "manuel.holtgrewe@bih-charite.de"
+
+__version__ = "0.2.1"
 
 __all__ = ["__version__"]
