2e6e0afeaab1f0b76bb5ca5aaf3614afd313ccc3
Till Hartmann <till.hartmann@bih-charite.de>
2024-11-27 11:30:27 +0100

    update pick_order list to vep >= 109

--- snappy_pipeline/models/annotation.py
+++ snappy_pipeline/models/annotation.py
@@ -25,7 +25,8 @@ class Vep(SnappyModel):
 
     pick_order: list[str] = [
         "biotype",
-        "mane",
+        "mane_select",
+        "mane_plus_clinial",
         "appris",
         "tsl",
         "ccds",
