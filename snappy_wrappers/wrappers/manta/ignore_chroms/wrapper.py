c61c3369db37905d068431c4edb87bfaafd125df
Eric Blanc <eric.blanc@bihealth.de>
2024-12-11 15:12:41 +0100

    feat: clean-up manta. TODO: enable exome & rna

---
+++ snappy_wrappers/wrappers/manta/ignore_chroms/wrapper.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""Wrapper for running Manta in somatic variant calling mode on WGS data"""
+
+import os
+
+from snappy_wrappers.simple_wrapper import SimpleWrapper
+from snappy_wrappers.tools.genome_windows import ignore_chroms
+
+__author__ = "Eric Blanc"
+__email__ = "eric.blanc@bih-charite.de"
+
+args = snakemake.params.get("args", {})
+
+bed = str(snakemake.output.callRegions).replace(".gz", "")
+
+with open(bed, "wt") as f:
+    for contig, length in ignore_chroms(str(snakemake.input.reference), set(args["ignore_chroms"]), return_ignored=False):
+        print(f"{contig}\t0\t{length}", file=f)
+
+cmd=f"bgzip {bed} ; tabix {snakemake.output.callRegions}"
+
+SimpleWrapper(snakemake, cmd).run_bash()
