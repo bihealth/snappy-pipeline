fd8a152e11a0898f6577b9a39445f7d0901eb7c9
Eric Blanc <eric.blanc@bihealth.de>
2024-12-19 16:07:48 +0100

    refactor: revert to piping into R rather than Rscript, because when using the pipe, each script line is echoed in the log

--- snappy_wrappers/snappy_wrapper.py
+++ snappy_wrappers/snappy_wrapper.py
@@ -163,7 +163,7 @@ class RWrapper(SnappyWrapper):
         with open(self._snakemake.log.script, "wt") as f:
             print(cmd, file=f)
         shell(SnappyWrapper.md5_log.format(log=self._snakemake.log.script))
-        self._run(f"Rscript --vanilla {self._snakemake.log.script}", None)
+        self._run(f"R --vanilla < {self._snakemake.log.script}", None)
 
     def run(self, cmd: str) -> None:
         self._run_R(cmd)
