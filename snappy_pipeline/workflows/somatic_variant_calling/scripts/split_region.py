48747c4f46f717e465e39eba038135f605809827
Till Hartmann <till.hartmann@bih-charite.de>
2025-02-06 16:04:28 +0100

    update split region script

--- snappy_pipeline/workflows/somatic_variant_calling/scripts/split_region.py
+++ snappy_pipeline/workflows/somatic_variant_calling/scripts/split_region.py
@@ -1,60 +1,122 @@
 import csv
-from bisect import bisect
-from contextlib import redirect_stderr
-from itertools import accumulate
+import fnmatch
+import sys
+from collections import namedtuple
+from contextlib import redirect_stderr, redirect_stdout
 
-from snappy_wrappers.tools.genome_windows import matches_any
-
-
-def split_regions(
-    contig_lengths: dict[str, int], n_chunks: int, offset: int = 1
-) -> list[list[[str, int, int]]]:
-    """Splits a given collection of contigs based on their length into `n_chunks` equal sized chunks"""
-    contigs, lengths = zip(*contig_lengths.items())
-    cumulative_lengths = list(accumulate(lengths, initial=0))
-    chunk_size = cumulative_lengths[-1] // n_chunks
-
-    all_entries = []
-    cumulative_start = cumulative_end = 0
-    for _ in range(n_chunks):
-        entries = []
-        idx = bisect(cumulative_lengths, cumulative_start) - 1
-        accumulated_length = 0
-        while accumulated_length < chunk_size and idx < len(contigs):
-            contig = contigs[idx]
-            available_length = cumulative_lengths[idx + 1] - cumulative_end
-            remaining = chunk_size - accumulated_length
-            cumulative_end = cumulative_start + min(available_length, remaining)
-
-            start = cumulative_start - cumulative_lengths[idx]
-            end = cumulative_end - cumulative_lengths[idx]
-            entries.append([contig, start + offset, end + offset])
-
-            diff = cumulative_end - cumulative_start
-            accumulated_length += diff
-            cumulative_start += diff
-            idx = bisect(cumulative_lengths, cumulative_start) - 1
-
-        assert sum(map(lambda x: x[2] - x[1], entries)) == chunk_size
-
-        all_entries.append(entries)
-
-    # make sure the last entry has any bases lost due to rounding
-    all_entries[-1][-1][2] = lengths[-1]
-    return all_entries
+from math import ceil
 
 
-with open(snakemake.log.log, "wt") as log:
-    with redirect_stderr(log):
-        with open(snakemake.params.fai, "rt") as fai:
+def matches_any(query, patterns):
+    for pattern in patterns:
+        if fnmatch.fnmatch(query, pattern):
+            return True
+    return False
+
+
+type Partition = list[tuple[str, int]]
+Interval = namedtuple("Interval", ["contig", "start", "end"])
+type Region = list[Interval]
+
+
+def partition_lengths_with_splits(
+    contigs: dict[str, int], num_partitions: int, slack: float = 0.1
+) -> list[Region]:
+    total_length = sum(contigs.values())
+    target_partition_size = ceil(total_length / num_partitions)
+    slack_threshold = slack * target_partition_size
+
+    partitions: list[Region] = [[] for _ in range(num_partitions)]
+    partition_sizes = [0] * num_partitions
+
+    sorted_contigs = list(sorted(contigs.items(), key=lambda x: x[1], reverse=True))
+
+    def calulate_end_position(start: int, length: int, contig_length: int) -> int:
+        return min(start + length, contig_length)
+
+    def add_interval(
+        partition_index: int, contig: str, start: int, length: int, contig_length: int
+    ):
+        end_position = calulate_end_position(start, length, contig_length)
+        partitions[partition_index].append(Interval(contig, start, end_position))
+        partition_sizes[partition_index] += length
+
+    for contig, length in sorted_contigs:
+        smallest = partition_sizes.index(min(partition_sizes))
+        remaining_space = target_partition_size - partition_sizes[smallest]
+
+        # Start position within the current contig
+        current_start = 0
+
+        if partition_sizes[smallest] + length > target_partition_size:
+            if remaining_space > slack_threshold:
+                add_interval(smallest, contig, current_start, remaining_space, length)
+                current_start += remaining_space
+                remaining_length = length - remaining_space
+
+                while remaining_length > 0:
+                    smallest = partition_sizes.index(min(partition_sizes))
+                    remaining_space = target_partition_size - partition_sizes[smallest]
+
+                    if remaining_space > 0:
+                        space_to_add = min(remaining_length, remaining_space)
+                        add_interval(smallest, contig, current_start, space_to_add, length)
+                        current_start += space_to_add
+                        remaining_length -= space_to_add
+                    else:
+                        break
+            else:
+                next_group_index = (smallest + 1) % num_partitions
+                add_interval(next_group_index, contig, current_start, length, length)
+        else:
+            add_interval(smallest, contig, current_start, length, length)
+
+    return partitions
+
+
+if __name__ == "__main__":
+    if snakemake := locals()["snakemake"]:
+        log = lambda _: open(snakemake.log.log, "wt")  # noqa: E731
+        fai_path = snakemake.params.fai
+        ignore_chroms = snakemake.params.ignore_chroms
+        padding = snakemake.params.padding
+        output_regions = snakemake.output.regions
+    else:
+        log = lambda: sys.stderr  # noqa: E731
+        fai_path = sys.argv[1]
+        ignore_chroms = sys.argv[3].split(",")
+        padding = int(sys.argv[4])
+        output_regions = sys.argv[5:]
+
+    with log() as log, redirect_stderr(log), redirect_stdout(log):
+        with open(fai_path, "rt") as fai:
             csv_reader = csv.reader(fai, delimiter="\t")
-            lengths = {
+            contigs = {
                 contig: int(length)
                 for contig, length, *_ in csv_reader
-                if not matches_any(contig, snakemake.params.ignore_chroms)
+                if not matches_any(contig, ignore_chroms)
             }
-        regions = split_regions(lengths, len(snakemake.output.regions))
-        for region, path in zip(regions, snakemake.output.regions):
+        num_regions = len(output_regions)
+        regions = partition_lengths_with_splits(contigs, num_regions)
+
+        for i, region in enumerate(regions):
+            size = sum(map(lambda r: r.end - r.start, region))
+            print(f"Region {i}, size {size}: {region}")
+
+        # Verify that all intervals for each contig sum up to the contig length:
+        for contig, length in contigs.items():
+            relevant_intervals = [
+                interval for region in regions for interval in region if interval.contig == contig
+            ]
+            total_length = sum(interval.end - interval.start for interval in relevant_intervals)
+            assert total_length == length, (
+                f"Total length of intervals for contig {contig} is {total_length}, expected {length}"
+            )
+
+        for region, path in zip(regions, output_regions):
             with open(path, "wt") as f:
-                for chrom, start, end in region:
-                    print(f"{chrom}:{start}-{end}", file=f)
+                for contig, start, end in region:
+                    contig_length = contigs[contig]
+                    padded_start = max(start - padding, 0)
+                    padded_end = min(end + padding, contig_length)
+                    print(f"{contig}:{padded_start}-{padded_end}", file=f)
