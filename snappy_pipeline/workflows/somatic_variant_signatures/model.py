8fa9baf5c685ef04d29c432befa10a04808a8c1e
Eric Blanc <eric.blanc@bihealth.de>
2025-03-06 19:05:16 +0100

    refactor: allow filtration to occur before annotation for steps that that take input from somatic_variant_calling, somatic_variant_annotation & somatic_variant_filtration

--- snappy_pipeline/workflows/somatic_variant_signatures/model.py
+++ snappy_pipeline/workflows/somatic_variant_signatures/model.py
@@ -1,3 +1,5 @@
+import enum
+
 from typing import Annotated
 
 from pydantic import Field
@@ -5,9 +7,20 @@ from pydantic import Field
 from snappy_pipeline.models import SnappyStepModel
 
 
-class SomaticVariantSignatures(SnappyStepModel):
-    is_filtered: bool = False
+class FiltrationSchema(enum.StrEnum):
+    unfiltered = "unfiltered"
+    list = "list"
+    sets = "sets"
 
+
+class FilterSet(enum.StrEnum):
+    NO_FILTER = "no_filter"
+    DKFZ_ONLY = "dkfz_only"
+    DKFZ_AND_EBFILTER = "dkfz_and_ebfilter"
+    DKFZ_AND_EBFILTER_AND_OXOG = "dkfz_and_ebfilter_and_oxog"
+
+
+class SomaticVariantSignatures(SnappyStepModel):
     path_somatic_variant: Annotated[str, Field(examples=["../somatic_variant_calling"])]
 
     tools_ngs_mapping: list[str] = []
@@ -19,11 +32,31 @@ class SomaticVariantSignatures(SnappyStepModel):
     tools_somatic_variant_annotation: list[str] = []
     """default to those configured for somatic_variant_annotation"""
 
-    filters: list[str] = []
+    has_annotation: bool = False
+    """Needed for building filenames only"""
+
+    filtration_schema: FiltrationSchema = FiltrationSchema.list
+    """Method of variant filtration (if any)"""
+
+    filter_before_annotation: bool = False
+    """Flag if filtration has been done before annotation"""
+
+    filter_sets: Annotated[
+        list[FilterSet], Field(None, deprecated="use `filter_list` instead")
+    ] = []
     """
-    When using variants after the somatic_variant_filtration step,
-    use "no_filter", "dkfz_only", "dkfz_and_ebfilter" or "dkfz_and_ebfilter_and_oxog"
+    DEPRECATED: use `filter_list instead`.
+    Set it to an empty value when using annotated variants without filtration.
     """
 
-    filtered_regions: list[str] = []
-    """When using variants after the somatic_variant_filtration step, use "genome_wide" or """ ""
+    exon_lists: Annotated[
+        list[str],
+        Field(
+            "genome_wide",
+            deprecated="Works together with filter_set, ignored when `filter_list` is selected",
+        ),
+    ] = []
+    """
+    DEPRECATED.
+    Works together with filter_set, ignored when "filter_list" is selected
+    """
--- snappy_pipeline/workflows/tumor_mutational_burden/model.py
+++ snappy_pipeline/workflows/tumor_mutational_burden/model.py
@@ -1,3 +1,5 @@
+import enum
+
 from typing import Annotated
 
 from pydantic import Field
@@ -5,11 +7,22 @@ from pydantic import Field
 from snappy_pipeline.models import SnappyStepModel
 
 
-class TumorMutationalBurden(SnappyStepModel):
-    has_annotation: bool
-    """TMB needs to know whether the vcf is annotated or not"""
+class FiltrationSchema(enum.StrEnum):
+    unfiltered = "unfiltered"
+    list = "list"
+    sets = "sets"
 
-    is_filtered: bool
+
+class FilterSet(enum.StrEnum):
+    NO_FILTER = "no_filter"
+    DKFZ_ONLY = "dkfz_only"
+    DKFZ_AND_EBFILTER = "dkfz_and_ebfilter"
+    DKFZ_AND_EBFILTER_AND_OXOG = "dkfz_and_ebfilter_and_oxog"
+
+
+class TumorMutationalBurden(SnappyStepModel):
+    has_annotation: bool = False
+    """Needed for building filenames only"""
 
     path_somatic_variant: Annotated[
         str, Field(examples=["../somatic_variant_annotation", "../somatic_variant_calling"])
@@ -25,14 +38,31 @@ class TumorMutationalBurden(SnappyStepModel):
     tools_somatic_variant_annotation: list[str] = []
     """default to those configured for somatic_variant_annotation"""
 
-    filters: list[str] = []
+    filtration_schema: FiltrationSchema = FiltrationSchema.list
+    """Method of variant filtration (if any)"""
+
+    filter_before_annotation: bool = False
+    """Flag if filtration has been done before annotation"""
+
+    filter_sets: Annotated[
+        list[FilterSet], Field(None, deprecated="use `filter_list` instead")
+    ] = []
     """
-    When using variants after the somatic_variant_filtration step,
-    use "no_filter", "dkfz_only", "dkfz_and_ebfilter" or "dkfz_and_ebfilter_and_oxog"
+    DEPRECATED: use `filter_list instead`.
+    Set it to an empty value when using annotated variants without filtration.
     """
 
-    filtered_regions: list[str] = []
-    """When using variants after the somatic_variant_filtration step, use "genome_wide" or "" """
+    exon_lists: Annotated[
+        list[str],
+        Field(
+            "genome_wide",
+            deprecated="Works together with filter_set, ignored when `filter_list` is selected",
+        ),
+    ] = []
+    """
+    DEPRECATED.
+    Works together with filter_set, ignored when "filter_list" is selected
+    """
 
     target_regions: str
     """Path to target_regions file (bed format)"""
