ebfcd9dc63866d654f250ca3430c9dad0b2e6bcd
Nicolai von Kügelgen <nicolai.von-kuegelgen@bih-charite.de>
2025-04-04 09:24:59 +0200

    fix: allow BaseStepPart get_resource to (again) deal with input-functions being defined by `get_resource_usage` (#614)

--- snappy_pipeline/workflows/helper_gcnv_model_wgs/__init__.py
+++ snappy_pipeline/workflows/helper_gcnv_model_wgs/__init__.py
@@ -194,7 +194,7 @@ class BuildGcnvWgsModelStepPart(BuildGcnvModelStepPart):
         """
         result = super().get_resource_usage(action)
 
-        def get_memory(wildcards, input=None, threads=None, attempt=None):
+        def get_memory(wildcards, input=None, threads=None, attempt=1):
             _, _, _ = wildcards, input, threads  # unused but cannot be renamed
             return f"{attempt * 4 * 1024 + 16 * 1024}M"
 
