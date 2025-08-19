2d821a58a5088586f66c1fda3c29debee3064233
Nicolai-vKuegelgen <nicolai.von-kuegelgen@bih-charite.de>
2025-08-19 16:15:07 +0200

    feat: Improve example config entries for MELT

--- snappy_pipeline/workflows/sv_calling_wgs/model.py
+++ snappy_pipeline/workflows/sv_calling_wgs/model.py
@@ -81,9 +81,9 @@ class Manta(SnappyModel):
 class Melt(SnappyModel):
     me_refs_infix: str = "1KGP_Hg19"
     me_types: list[str] = ["ALU", "LINE1", "SVA"]
-    me_refs_path: str
-    jar_file: str
-    genes_file: str = "add_bed_files/1KGP_Hg19 / hg19.genes.bed"
+    me_refs_path: str = Field(examples=['/path/to/melt/support/files'])
+    jar_file: str = Field(examples=["/path/to/MELTv2.2.2/MELT.jar"])
+    genes_file: str = Field(examples=["/path/to/MELTv2.2.2/add_bed_files/1KGP_Hg19/hg19.genes.bed"])
     """adjust, e.g., Hg38/Hg38.genes.bed"""
 
     skip_libraries: list[str] = []
