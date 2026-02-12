c802f54d62592a9c493d26695e970b11ad9d6167
Eric Blanc <eric.blanc@bihealth.de>
2026-02-12 10:40:30 +0100

    feat: added derived classes for germline variant annotation & filtration

---
+++ snappy_pipeline/workflows/germline_variant_filtration/__init__.py
@@ -0,0 +1,5 @@
+from snappy_pipeline.workflows.any_variant_filtration import AnyVariantFiltrationWorkflow
+
+
+class GermlineVariantFiltrationWorkflow(AnyVariantFiltrationWorkflow):
+    name = "germline_variant_filtration"
