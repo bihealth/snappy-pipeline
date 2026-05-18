d98ccacbb52d294ca4ebd7e9dc744fd91a1be421
Till Hartmann <till.hartmann@bih-charite.de>
2026-05-18 16:55:38 +0200

    remove module imports handled by main orchestrator

--- snappy_pipeline/workflows/somatic_targeted_seq_cnv_calling/__init__.py
+++ snappy_pipeline/workflows/somatic_targeted_seq_cnv_calling/__init__.py
@@ -742,7 +742,7 @@ class SomaticTargetedSeqCnvCallingWorkflow(BaseStep):
         )
         # Initialize sub-workflows
         self.register_module("ngs_mapping")
-        if self.config.tool == "purecn":
+        if str(self.config.tool) == "purecn":
             self.register_module(
                 "somatic_variant_calling",
                 self.config.purecn.path_somatic_variants,
