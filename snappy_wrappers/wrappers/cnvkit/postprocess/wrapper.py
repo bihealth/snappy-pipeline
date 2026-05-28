4686a1884484db4efe73780c50c739c464173710
Till Hartmann <till.hartmann@bih-charite.de>
2026-05-28 18:53:25 +0200

    11th batch of wrapper migrations

--- snappy_wrappers/wrappers/cnvkit/postprocess/wrapper.py
+++ snappy_wrappers/wrappers/cnvkit/postprocess/wrapper.py
@@ -1,30 +1,13 @@
 # -*- coding: utf-8 -*-
 """Wrapper vor cnvkit.py call"""
 
-from snakemake.shell import shell
+from snappy_wrappers.snappy_wrapper import ShellWrapper
 
 __author__ = "Manuel Holtgrewe"
 __email__ = "manuel.holtgrewe@bih-charite.de"
 
-shell(
+ShellWrapper(snakemake).run(
     r"""
-# Also pipe everything to log file
-if [[ -n "{snakemake.log.log}" ]]; then
-    if [[ "$(set +e; tty; set -e)" != "" ]]; then
-        rm -f "{snakemake.log.log}" && mkdir -p $(dirname {snakemake.log.log})
-        exec &> >(tee -a "{snakemake.log.log}" >&2)
-    else
-        rm -f "{snakemake.log.log}" && mkdir -p $(dirname {snakemake.log.log})
-        echo "No tty, logging disabled" >"{snakemake.log.log}"
-    fi
-fi
-
-# Write out information about conda installation.
-conda list >{snakemake.log.conda_list}
-conda info >{snakemake.log.conda_info}
-md5sum {snakemake.log.conda_list} >{snakemake.log.conda_list_md5}
-md5sum {snakemake.log.conda_info} >{snakemake.log.conda_info_md5}
-
 set -x
 
 # -----------------------------------------------------------------------------
@@ -73,18 +56,5 @@ segments <- segments[,dna_copy_columns]
 
 write.table(segments, file="{snakemake.output.final}", sep="\t", col.names=TRUE, row.names=FALSE, quote=FALSE)
 _EOF
-
-d=$(dirname "{snakemake.output.final}")
-pushd $d
-fn=$(basename "{snakemake.output.final}")
-md5sum $fn > $fn.md5
-popd
-"""
-)
-
-# Compute MD5 sums of logs.
-shell(
-    r"""
-md5sum {snakemake.log.log} >{snakemake.log.log_md5}
 """
 )
--- snappy_wrappers/wrappers/cnvkit/report/wrapper.py
+++ snappy_wrappers/wrappers/cnvkit/report/wrapper.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """Wrapper for cnvkit.py report"""
 
-from snakemake.shell import shell
+from snappy_wrappers.snappy_wrapper import ShellWrapper
 
 __author__ = "Manuel Holtgrewe"
 __email__ = "manuel.holtgrewe@bih-charite.de"
@@ -26,7 +26,6 @@ if getattr(snakemake.output, "breaks", ""):
         )
     else:
         breaks = "touch {out}".format(out=snakemake.output.breaks)
-    breaks += "\n" + "md5 {out}".format(out=snakemake.output.breaks)
 else:
     breaks = ""
 
@@ -52,7 +51,6 @@ cnvkit.py genemetrics \
         )
     else:
         genemetrics = "touch {out}".format(out=snakemake.output.genemetrics)
-    genemetrics += "\n" + "md5 {out}".format(out=snakemake.output.genemetrics)
 else:
     genemetrics = ""
 
@@ -78,7 +76,6 @@ cnvkit.py segmetrics \
         )
     else:
         segmetrics = "touch {out}".format(out=snakemake.output.segmetrics)
-    segmetrics += "\n" + "md5 {out}".format(out=snakemake.output.segmetrics)
 else:
     segmetrics = ""
 
@@ -99,7 +96,6 @@ if getattr(snakemake.output, "sex", ""):
         )
     else:
         sex = "touch {out}".format(out=snakemake.output.sex)
-    sex += "\n" + "md5 {out}".format(out=snakemake.output.sex)
 else:
     sex = ""
 
@@ -121,43 +117,12 @@ cnvkit.py metrics \
         )
     else:
         metrics = "touch {out}".format(out=snakemake.output.metrics)
-    metrics += "\n" + "md5 {out}".format(out=snakemake.output.metrics)
 else:
     metrics = ""
 
-shell(
+ShellWrapper(snakemake).run(
     r"""
-# Also pipe everything to log file
-if [[ -n "{snakemake.log.log}" ]]; then
-    if [[ "$(set +e; tty; set -e)" != "" ]]; then
-        rm -f "{snakemake.log.log}" && mkdir -p $(dirname {snakemake.log.log})
-        exec &> >(tee -a "{snakemake.log.log}" >&2)
-    else
-        rm -f "{snakemake.log.log}" && mkdir -p $(dirname {snakemake.log.log})
-        echo "No tty, logging disabled" >"{snakemake.log.log}"
-    fi
-fi
-
-# Write out information about conda installation.
-conda list >{snakemake.log.conda_list}
-conda info >{snakemake.log.conda_info}
-md5sum {snakemake.log.conda_list} >{snakemake.log.conda_list_md5}
-md5sum {snakemake.log.conda_info} >{snakemake.log.conda_info_md5}
-
 set -x
-
-# -----------------------------------------------------------------------------
-
-md5()
-{{
-    d=$(dirname $1)
-    f=$(basename $1)
-    pushd $d
-    md5sum $f > $f.md5
-    popd
-}}
-
-# -----------------------------------------------------------------------------
 
 {breaks}
 
@@ -171,9 +136,3 @@ md5()
 """
 )
 
-# Compute MD5 sums of logs.
-shell(
-    r"""
-md5sum {snakemake.log.log} >{snakemake.log.log_md5}
-"""
-)
