303b4623c325e2ed7373ace36a0634245c131355
Till Hartmann <till.hartmann@bih-charite.de>
2026-05-28 18:47:26 +0200

    9th batch of wrapper migrations

--- snappy_wrappers/wrappers/cnvetti/wgs/coverage/wrapper.py
+++ snappy_wrappers/wrappers/cnvetti/wgs/coverage/wrapper.py
@@ -1,39 +1,15 @@
 # -*- coding: utf-8 -*-
 """Wrapper for running CNVetti WGS coverage step."""
 
-from snakemake.shell import shell
+from snappy_wrappers.snappy_wrapper import ShellWrapper
 
 __author__ = "Manuel Holtgrewe <manuel.holtgrewe@bih-charite.de>"
 
 args = getattr(snakemake.params, "args", {})
 
-shell(
+ShellWrapper(snakemake).run(
     r"""
 set -x
-
-# Write out information about conda installation --------------------------------------------------
-
-conda list >{snakemake.log.conda_list}
-conda info >{snakemake.log.conda_info}
-
-# Also pipe stderr to log file --------------------------------------------------------------------
-
-if [[ -n "{snakemake.log.log}" ]]; then
-    if [[ "$(set +e; tty; set -e)" != "" ]]; then
-        rm -f "{snakemake.log.log}" && mkdir -p $(dirname {snakemake.log.log})
-        exec 2> >(tee -a "{snakemake.log.log}" >&2)
-    else
-        rm -f "{snakemake.log.log}" && mkdir -p $(dirname {snakemake.log.log})
-        echo "No tty, logging disabled" >"{snakemake.log.log}"
-    fi
-fi
-
-# Setup auto-cleaned TMPDIR -----------------------------------------------------------------------
-
-export TMPDIR=$(mktemp -d)
-trap "rm -rf $TMPDIR" EXIT
-
-# Compute coverage and normalize ------------------------------------------------------------------
 
 REF={args[reference]}
 
@@ -51,11 +27,5 @@ cnvetti cmd normalize \
     --normalization {args[normalization]} \
     --input $TMPDIR/cov.bcf \
     --output {snakemake.output.bcf}
-
-# Compute MD5 checksums ---------------------------------------------------------------------------
-
-pushd $(dirname "{snakemake.output.bcf}")
-md5sum $(basename "{snakemake.output.bcf}") >$(basename "{snakemake.output.bcf}").md5
-md5sum $(basename "{snakemake.output.csi}") >$(basename "{snakemake.output.csi}").md5
 """
 )
