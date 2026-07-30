b91bd1062047ee73a9084b708c8fd1b21df7d99f
Till Hartmann <till.hartmann@tu-dortmund.de>
2026-07-30 12:34:53 +0200

    remove unused workflow

--- snappy_pipeline/workflow_registry.py
+++ snappy_pipeline/workflow_registry.py
@@ -37,7 +37,6 @@ from snappy_pipeline.workflows.somatic_targeted_seq_cnv_calling import (
 )
 from snappy_pipeline.workflows.somatic_variant_signatures import SomaticVariantSignaturesWorkflow
 from snappy_pipeline.workflows.somatic_wgs_cnv_calling import SomaticWgsCnvCallingWorkflow
-from snappy_pipeline.workflows.somatic_wgs_sv_calling import SomaticWgsSvCallingWorkflow
 from snappy_pipeline.workflows.sv_calling_targeted import SvCallingTargetedWorkflow
 from snappy_pipeline.workflows.sv_calling_wgs import SvCallingWgsWorkflow
 from snappy_pipeline.workflows.targeted_seq_mei_calling import MeiWorkflow
@@ -83,7 +82,6 @@ WORKFLOW_REGISTRY = {
     "somatic_targeted_seq_cnv_calling": SomaticTargetedSeqCnvCallingWorkflow,
     "somatic_variant_signatures": SomaticVariantSignaturesWorkflow,
     "somatic_wgs_cnv_calling": SomaticWgsCnvCallingWorkflow,
-    "somatic_wgs_sv_calling": SomaticWgsSvCallingWorkflow,
     "sv_calling_targeted": SvCallingTargetedWorkflow,
     "sv_calling_wgs": SvCallingWgsWorkflow,
     "targeted_seq_mei_calling": MeiWorkflow,
